253242823efb88ce7d581e7bd9c68f81be5b503f
Alderaeney <sariaaskort@tuta.io>
2020-09-27 18:50:26 +0200

    Changed opensuse tumbleweed config

--- laptop_opensuse_config.sh
+++ laptop_opensuse_config.sh
@@ -9,10 +9,25 @@ zypper ar -cfp 99 http://download.opensuse.org/repositories/shells:/zsh-users:/z
 zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/openSUSE_Tumbleweed/shells:zsh-users:zsh-autosuggestions.repo
 zypper ar -cfp 99 https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/ emulators
 
+#Installing nvidia drivers
+OneClickInstallCLI https://www.opensuse-community.org/nvidia_G05.ymp
+
+prime-select intel2
+
+# Installing intel-undervolt
+OneClickInstallCLI https://software.opensuse.org/ymp/hardware/openSUSE_Tumbleweed/intel-undervolt.ymp
+
+# Setting up intel-undervolt
+sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 0 'GPU' 0/undervolt 0 'GPU' -100/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
+systemctl enable intel-undervolt
+
+
 zypper refresh
 
 # Installing packages
-zypper in chromium steam lutris metatheme-plata-common telegram-theme-plata papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions tilix mpv rhythmbox dolphin-emu discord telegram-desktop nextcloud-client flatpak retroarch gamemoded
+zypper in chromium steam lutris metatheme-plata-common telegram-theme-plata papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions tilix mpv rhythmbox dolphin-emu discord telegram-desktop nextcloud-client flatpak gamemoded
 
 # Adjusting sound quality
 sed -i "s/; enable-lfe-remixing = no.*/enable-lfe-remixing = yes/" /etc/pulse/daemon.conf
@@ -25,8 +40,8 @@ sed -i "s/; alternate-sample-rate = 48000.*/alternate-sample-rate = 48000/" /etc
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Installing flatpak apps
-flatpak install flathub com.mojang.Minecraft com.katawa_shoujo.KatawaShoujo
+flatpak install flathub com.mojang.Minecraft 
 
 # Installing prime offload launchers
-cp prime* /usr/bin
-chmod +x /usr/bin/prime*
+cp prime-run /usr/bin
+chmod +x /usr/bin/prime-run

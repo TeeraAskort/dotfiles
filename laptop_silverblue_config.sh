d3d078438b0e006dd157a0aabb2fc95339ad40a5
Andreu Furió Benavent <sariaaskort@tuta.io>
2020-09-29 18:52:19 +0000

    laptop_silverblue_config.sh edited online with Bitbucket

--- laptop_silverblue_config.sh
+++ laptop_silverblue_config.sh
@@ -4,10 +4,10 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Installing flathub applications
-flatpak install flathub org.telegram.desktop com.discordapp.Discord org.DolphinEmu.dolphin-emu com.jetbrains.IntelliJ-IDEA-Community com.google.AndroidStudio io.github.celluloid_player.Celluloid com.nextcloud.desktopclient.nextcloud org.gnome.Rhythmbox3
+flatpak install flathub org.telegram.desktop com.discordapp.Discord org.DolphinEmu.dolphin-emu com.jetbrains.IntelliJ-IDEA-Community com.google.AndroidStudio io.github.celluloid_player.Celluloid com.nextcloud.desktopclient.nextcloud org.gnome.Rhythmbox3 com.transmissionbt.Transmission
 
 # Adding rpmfusion repos
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Installing packages
-rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks tilix thermald
+rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks tilix 

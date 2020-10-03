c5fbe90937d043ac176d84c6fda9054b09c5f238
Alderaeney <sariaaskort@tuta.io>
2020-10-03 21:41:13 +0200

    Enabled intel-undervolt service

--- home_folder.sh
+++ home_folder.sh
@@ -31,6 +31,7 @@ cd .. && sudo rm -r intel-undervolt
 sudo sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100/g" /etc/intel-undervolt.conf
 sudo sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -100/g" /etc/intel-undervolt.conf
 sudo sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
+sudo systemctl enable intel-undervolt
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

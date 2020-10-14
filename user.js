ae097db6c6918a3937804771384bb2775d29d26d
Alderaeney <sariaaskort@tuta.io>
2020-10-14 16:28:32 +0200

    Added privacy settings to firefox

--- user.js
+++ user.js
@@ -3,3 +3,4 @@ user_pref("media.hardwaremediakeys.enabled", true);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("media.ffmpeg.vaapi-drm-display.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("privacy.firstparty.isolate", true);

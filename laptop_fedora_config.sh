b1db6d5d71e6114c3fa07f2cd6644071165d79a0
Alderaeney <sariaaskort@tuta.io>
2020-10-19 17:33:50 +0200

    Added fastestmirror to fedora config

--- laptop_fedora_config.sh
+++ laptop_fedora_config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Add fastestmirror to dnf configuration
+echo "fastestmirror=1" | tee -a /etc/dnf/dnf.conf
+
 #Install RPMfusion
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 

f6ea01db7697a380cbd1da7cb96b4961a846c88d
Alderaeney <sariaaskort@tuta.io>
2020-10-24 16:15:08 +0200

    Changed arch config

--- laptop_arch_config.sh
+++ laptop_arch_config.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Installing required packages
-sudo pacman -S tilix mpv rhythmbox jdk-openjdk dolphin-emu discord telegram-desktop flatpak code wine winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam
+sudo pacman -S tilix mpv rhythmbox jdk11-openjdk dolphin-emu discord telegram-desktop flatpak code wine winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam intellij-idea-community-edition thermald tlp earlyoom systembus-notify apparmor
+
+# Enabling services
+sudo systemctl enable thermald tlp earlyoom apparmor
+
+# Add apparmor kernel paramenters
+sudo sed -ie 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 apparmor=1 lsm=lockdown,yama,apparmor"/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Adjusting sound quality
 sudo sed -i "s/; enable-lfe-remixing = no.*/enable-lfe-remixing = yes/" /etc/pulse/daemon.conf
@@ -16,10 +23,15 @@ sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
 # Installing AUR packages
 yay -S dxvk-bin aic94xx-firmware wd719x-firmware plata-theme-bin nerd-fonts-fantasque-sans-mono intel-undervolt gamemode lib32-gamemode
 
+# Configuring intel-undervolt
+sudo sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100/g" /etc/intel-undervolt.conf
+sudo sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -100/g" /etc/intel-undervolt.conf
+sudo sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
+sudo systemctl enable intel-undervolt
+
 # Adding flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Installing flatpak applications
 flatpak install flathub com.mojang.Minecraft
 
-echo GAMEMODERUNEXEC=prime-run | sudo tee -a /etc/environment

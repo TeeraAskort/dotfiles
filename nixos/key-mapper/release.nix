35f01ba9c7e8f6edf2daddbce8757e098f9cb5eb
Alderaeney <sariaaskort@tuta.io>
2020-12-31 07:53:48 +0100

    Changed key-mapper nixos config

--- nixos/key-mapper/release.nix
+++ nixos/key-mapper/release.nix
@@ -1,24 +1,61 @@
-{ lib, python39Packages }:
+{ lib, python3Packages, fetchFromGitHub
+, gobject-introspection
+, python3
+, wrapGAppsHook
+, pango
+, gdk-pixbuf
+, atk
+, coreutils
+, xmodmap }:
 
-python39Packages.buildPythonApplication rec {
+let 
+
+binPath = lib.makeBinPath [
+    coreutils
+    xmodmap
+];
+
+in python3Packages.buildPythonApplication rec {
     pname = "key-mapper-git";
     version = "0.4.0";
     
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
         owner = "sezanzeb";
         repo = "key-mapper";
-        rev = "d457b5efbecd79d1ee6cfe3e1172f392731b829e";
-        sha256 = "0732a86523ce22cd0146a58605f2141c17b6cca34b3a5632971f9539089e3e3e";
+        rev = version;
+        sha256 = "1ssgfp7rh4q8bbkxhsp2l63fvwhn2bhfkd2hxfvgnibbfp8r5vk7";
     };
-    
-   buildInputs = with python39Packages; [ pydbus pygobject evdev ];
 
-   propagateBuildInputs = with python39Packages; [ pydbus pygobject evdev ];
+    nativeBuildInputs = [ gobject-introspection wrapGAppsHook pango gdk-pixbuf atk ];
+    
+    buildInputs = [
+      (python3.withPackages (ps: with ps; [ pydbus pygobject3 evdev setuptools ]))
+      gobject-introspection
+      pango
+      gdk-pixbuf
+      atk
+    ];
+
+    propagateBuildInputs = with python3Packages; [ pydbus pygobject3 evdev setuptools ];
+
+    doCheck = false;
+
+    dontWrapGApps = true;
+    makeWrapperArgs = [
+        "--prefix PATH : ${binPath}"
+        ''''${gappsWrapperArgs[@]}''
+    ];
+    
+    strictDeps = false;
+
+    postInstall = ''
+      mv $out/lib/python*/site-packages/usr/bin $out
+    '';
 
     meta = with lib; {
         homepage = "https://github.com/sezanzeb/key-mapper";
         description = "Key mapping tool";
-        license = licenses.gpl3;
+        license = licenses.gpl3Only;
         maintainers = with maintainers; [ Alderaeney ];
     };
 }

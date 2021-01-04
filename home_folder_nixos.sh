222af03db8a1e1e032949b6f497f3087ea86d664
Alderaeney <sariaaskort@tuta.io>
2021-01-04 19:21:53 +0100

    Added home_folder config for nixos

---
+++ home_folder_nixos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -r ~/Descargas ~/Documentos ~/Escritorio ~/Música ~/Imágenes ~/Downloads ~/Torrent
+
+ln -s /home/link/Datos/Descargas /home/link
+ln -s /home/link/Datos/Descargas /home/link/Downloads
+ln -s /home/link/Datos/Documentos /home/link
+ln -s /home/link/Datos/Escritorio /home/link
+ln -s /home/link/Datos/Música /home/link
+ln -s /home/link/Datos/Imágenes /home/link
+ln -s /home/link/Datos/Torrent /home/link
+ln -s /home/link/Datos/Nextcloud /home/link
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp dotfiles/.vimrc ~
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
+
+echo "source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" | tee -a ~/.zshrc
+
+cp nixos/home.nix ~/.config/nixpkgs/home.nix
+
+home-manager switch

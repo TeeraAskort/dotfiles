d60bd97cae703b3babdcbc46b94552b91b4d34a8
Alderaeney <sariaaskort@tuta.io>
2021-01-12 17:42:44 +0100

    Added missing packages to kinoite config

--- fedora-centos/laptop_kinoite_config.sh
+++ fedora-centos/laptop_kinoite_config.sh
@@ -2,8 +2,7 @@
 
 # Adding repos
 curl -L "https://copr.fedorainfracloud.org/coprs/dawid/better_fonts/repo/fedora-33/dawid-better_fonts-fedora-33.repo" > better_fonts.repo
-curl -L "https://copr.fedorainfracloud.org/coprs/alderaeney/plata-theme-master/repo/fedora-33/alderaeney-plata-theme-master-fedora-33.repo" > plata-theme-master.repo
-cp better_fonts.repo plata-theme-master.repo /etc/yum.repos.d/
+cp better_fonts.repo /etc/yum.repos.d/
 
 # Update system
 rpm-ostree upgrade
@@ -22,7 +21,7 @@ flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kde
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install apps
-rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks tilix intel-undervolt strawberry fontconfig-font-replacements fontconfig-enhanced-defaults openssl papirus-icon-theme
+rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks tilix intel-undervolt strawberry fontconfig-font-replacements fontconfig-enhanced-defaults openssl papirus-icon-theme net-tools libsnl
 
 # Installing flatpak apps
 flatpak install flathub org.telegram.desktop com.discordapp.Discord org.DolphinEmu.dolphin-emu com.nextcloud.desktopclient.nextcloud org.qbittorrent.qBittorrent org.libreoffice.LibreOffice com.valvesoftware.Steam com.github.Eloston.UngoogledChromium org.freedesktop.Piper com.github.micahflee.torbrowser-launcher com.google.AndroidStudio com.vscodium.codium org.gnome.Evolution org.jdownloader.JDownloader org.gimp.GIMP io.lbry.lbry-app com.mojang.Minecraft

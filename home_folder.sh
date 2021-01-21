2b0623e2816045ae7b22e6b0b69c390484cdf939
Alderaeney <sariaaskort@tuta.io>
2021-01-21 19:42:25 +0100

    Added neovim config and packages to debian gnome

--- home_folder.sh
+++ home_folder.sh
@@ -63,7 +63,7 @@ pulseaudio -k
 
 cp dotfiles/.vimrc ~
 mkdir -p ~/.config/nvim/
-cp dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/mpv/shaders/
 curl -LO https://gist.githubusercontent.com/igv/36508af3ffc84410fe39761d6969be10/raw/ac09db2c0664150863e85d5a4f9f0106b6443a12/SSimDownscaler.glsl

99c4827b1f25263dbd5eb50f6c470c1cf0405cd2
Alderaeney <sariaaskort@tuta.io>
2021-02-13 10:51:46 +0000

    Changed hostname

--- nixos/setup_luks_fido.sh
+++ nixos/setup_luks_fido.sh
@@ -1,6 +1,6 @@
 nix-env -iA nixos.fido2luks
 
-export FIDO2_LABEL="/dev/nvme0n1p2 @ $HOSTNAME" 
+export FIDO2_LABEL="/dev/nvme0n1p2 @ link-gl63-8rc" 
 cred=$(fido2luks credential "$FIDO2_LABEL")
 
 fido2luks -i add-key /dev/nvme0n1p2 $cred

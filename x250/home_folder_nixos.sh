a4bbb4a8c1ac0425148a992d5cab03aedfb7db58
Alderaeney <sariaaskort@tuta.io>
2021-02-19 18:46:52 +0100

    Commented home_folder for nixos; Added neovim config

--- x250/home_folder_nixos.sh
+++ x250/home_folder_nixos.sh
@@ -4,6 +4,7 @@ _script="$(readlink -f ${BASH_SOURCE[0]})"
 
 directory="$(dirname $_script)"
 
+## Configuring home folders
 sudo rm -r ~/Descargas ~/Documentos ~/Escritorio ~/Música ~/Imágenes ~/Downloads ~/Torrent
 
 ln -s /home/link/Datos/Descargas /home/link
@@ -15,27 +16,42 @@ ln -s /home/link/Datos/Imágenes /home/link
 ln -s /home/link/Datos/Torrent /home/link
 ln -s /home/link/Datos/Nextcloud /home/link
 
+## Downloading Plug for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+## Configuring vim/neovim
+cp $directory/dotfiles/.vimrc ~
+mkdir -p ~/.config/nvim/
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+nvim +PlugInstall +q +q
+cd ~/.vim/plugged/youcompleteme 
+python install.py 
+
+## Copying fonts
 mkdir ~/.fonts
 cd ~/.fonts && unzip ~/Documentos/fonts.zip
 
-cp $directory/dotfiles/.vimrc ~
-
+## Configuring mpv
 mkdir -p ~/.config/mpv/shaders/
 curl -LO https://gist.githubusercontent.com/igv/36508af3ffc84410fe39761d6969be10/raw/ac09db2c0664150863e85d5a4f9f0106b6443a12/SSimDownscaler.glsl
 curl -LO https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/424a8deae7d5a142d0bbbf1552a686a0421644ad/KrigBilateral.glsl
 mv SSimDownscaler.glsl KrigBilateral.glsl ~/.config/mpv/shaders
 cp $directory/dotfiles/mpv.conf ~/.config/mpv/
 
+## Configuring git
 git config --global user.name "Alderaeney"
 git config --global user.email "sariaaskort@tuta.io"
 git config --global init.defaultBranch master
 
+## Installing flatpak applications
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub io.lbry.lbry-app com.mojang.Minecraft com.tutanota.Tutanota com.github.micahflee.torbrowser-launcher org.jdownloader.JDownloader
 
+## Configuring u2f authentication
 mkdir -p ~/.config/Yubico
 
 echo "Insert FIDO2 card and press a key:"

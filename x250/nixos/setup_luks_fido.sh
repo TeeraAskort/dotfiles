0e93cff62c3872a84d7be70decf5a5c2faa5fcbd
Alderaeney <sariaaskort@tuta.io>
2021-02-20 14:27:56 +0000

    Fixed fido setup partition

--- x250/nixos/setup_luks_fido.sh
+++ x250/nixos/setup_luks_fido.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rootDisk=$(lsblk -io KNAME,TYPE,MODEL | grep disk | grep TS128GMTS430S | cut -d" " -f1)
+
 nix-env -iA nixos.fido2luks
 
 echo "Insert FIDO2 card and press a key:"
@@ -7,13 +9,13 @@ read -n 1
 
 echo "Tap on your FIDO2 device"
 
-export FIDO2_LABEL="/dev/sdb2 @ link-x250" 
+export FIDO2_LABEL="/dev/${rootDisk}2 @ link-x250" 
 until cred=$(fido2luks credential "$FIDO2_LABEL")
 do
 	echo "There has been a problem creating the credentials for fido2luks key"
 done
 
-until fido2luks -i add-key /dev/sdb2 $cred
+until fido2luks -i add-key /dev/${rootDisk}2 $cred
 do
 	echo "There has been a problem adding fido key to luks"
 done

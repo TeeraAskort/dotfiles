dfcd28fbf94e2ccf2c83899743d6c4f4a7045dba
Alderaeney <sariaaskort@tuta.io>
2021-02-21 08:12:26 +0100

    Removed mpv shaders

--- x250/home_folder.sh
+++ x250/home_folder.sh
@@ -90,10 +90,7 @@ python install.py
 cp $directory/dotfiles/chromium-flags.conf ~/.config
 
 ## Configuring mpv
-mkdir -p ~/.config/mpv/shaders/
-curl -LO https://gist.githubusercontent.com/igv/36508af3ffc84410fe39761d6969be10/raw/ac09db2c0664150863e85d5a4f9f0106b6443a12/SSimDownscaler.glsl
-curl -LO https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/424a8deae7d5a142d0bbbf1552a686a0421644ad/KrigBilateral.glsl
-mv SSimDownscaler.glsl KrigBilateral.glsl ~/.config/mpv/shaders
+mkdir -p ~/.config/mpv/
 cp $directory/dotfiles/mpv.conf ~/.config/mpv/
 
 ## Configuring u2f cards

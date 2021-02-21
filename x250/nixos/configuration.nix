3ef0a4082206f21544d4f164d6b91810ec160194
Alderaeney <sariaaskort@tuta.io>
2021-02-21 08:33:38 +0000

    Workaround for streamer plugins

--- x250/nixos/configuration.nix
+++ x250/nixos/configuration.nix
@@ -47,26 +47,7 @@ in
   time.timeZone = "Europe/Madrid";
 
   # Allow nonfree packages
-  nixpkgs.config = {
-    allowUnfree = true;
-
-    packageOverrides = super: let self = super.pkgs; in {
-      strawberry = super.strawberry = {
-        override { withGstreamer = true; };
-
-        overrideAttrs (attrs: {
-          withGstreamer = attrs.withGstreamer ++ [
-            self.gst_all_1.gst-plugins-bad
-            self.gst_all_1.gst-plugins-ugly
-	    self.gst_all_1.gstreamer
-            self.gst_all_1.gst-plugins-base
-            self.gst_all_1.gst-plugins-good
-            self.gst_all_1.gst-plugins-ugly
-	  ];
-        });
-      };
-    };
-  };
+  nixpkgs.config.allowUnfree = true;
 
   # Package overrides
   nixpkgs.config.packageOverrides = pkgs: {
@@ -88,11 +69,14 @@ in
     nextcloud-client parallel
     adwaita-qt ffmpeg-full nodejs nodePackages.npm
     python39Packages.pynvim neovim cmake python39Full gcc gnumake
+    gst_all_1.gstreamer gst_all_1.gst-vaapi gst_all_1.gst-libav 
+    gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gst_all_1.gst-plugins-good gst_all_1.gst-plugins-base
   ];
 
   # Environment variables
   environment.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
+    GST_PLUGIN_PATH = "/nix/var/nix/profiles/system/sw/lib/gstreamer-1.0";
   };
 
   # QT5 Theming

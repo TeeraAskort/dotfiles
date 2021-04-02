bc7d728c4bf6436ada341e50d3c585b14586cf2f
Alderaeney <alderaeney@alderaeney.com>
2021-04-02 19:49:36 +0200

    Added hardware acceleration to arch configs

--- x250/arch/laptop_arch_xfce.sh
+++ x250/arch/laptop_arch_xfce.sh
@@ -57,7 +57,7 @@ sed -i '/\[multilib\]/{n;s/^#//g}' /etc/pacman.conf
 pacman -Syu --noconfirm
 
 # Installing drivers 
-pacman -S --noconfirm lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S --noconfirm lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader libva-intel-driver intel-media-driver xf86-video-intel
 
 # Installing services
 pacman -S --noconfirm  networkmanager openssh xdg-user-dirs haveged intel-ucode bluez bluez-libs

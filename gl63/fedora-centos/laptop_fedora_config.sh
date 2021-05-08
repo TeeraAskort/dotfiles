68d32c56aaa9c86edb1a8d335233bb3e84b432c9
Alderaeney <alderaeney@alderaeney.com>
2021-05-08 10:40:16 +0200

    Added materia themeA

--- gl63/fedora-centos/laptop_fedora_config.sh
+++ gl63/fedora-centos/laptop_fedora_config.sh
@@ -41,7 +41,7 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 dnf upgrade -y
 
 #Install required packages
-dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu pcsx2 fontconfig-enhanced-defaults fontconfig-font-replacements intel-undervolt ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 strawberry NetworkManager-l2tp-gnome google-chrome-stable mednafen mednaffe youtube-dl kernel-xanmod-edge kernel-xanmod-edge-devel pam-u2f pamu2fcfg libva-intel-hybrid-driver
+dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu pcsx2 fontconfig-enhanced-defaults fontconfig-font-replacements intel-undervolt ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 strawberry NetworkManager-l2tp-gnome google-chrome-stable mednafen mednaffe youtube-dl kernel-xanmod-edge kernel-xanmod-edge-devel pam-u2f pamu2fcfg libva-intel-hybrid-driver materia-gtk-theme materia-kde
 
 systemctl enable thermald
 

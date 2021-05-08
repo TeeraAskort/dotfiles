6a82e767d7f2c4eea73b41f8183d78f87b4f9e57
Alderaeney <alderaeney@alderaeney.com>
2021-05-08 16:16:37 +0200

    Added xfce installation

--- gl63/arch/laptop_arch_xfce.sh
+++ gl63/arch/laptop_arch_xfce.sh
@@ -18,20 +18,63 @@ hwclock --systohc
 echo link-gl63-8rc > /etc/hostname
 
 # Root password
-clear
-echo "Enter root password"
-passwd
+#clear
+#echo "Enter root password"
+#until passwd
+#do
+#	echo "Enter the password correctly"
+#done
+
+# Fixing faillock
+sed -i "s/# deny = 3/deny = 0/g" /etc/security/faillock.conf
+
+# Restricting root login
+sed -i "/pam_wheel.so use_uid/ s/^#//g" /etc/pam.d/su
+sed -i "/pam_wheel.so use_uid/ s/^#//g" /etc/pam.d/su-l
 
 # Create user
 clear
 useradd -m -g users -G wheel -s /bin/bash link
 echo "Enter link's password"
-passwd link
+until passwd link
+do
+	echo "Enter the password correctly"
+done
 
 # Sudo configuration
 EDITOR=vim visudo
 
-# Installing yay
+# Enabling colors in pacman
+sed -i "s/#Color/Color/g" /etc/pacman.conf
+
+# Enabling multilib repo
+sed -i '/\[multilib\]/s/^#//g' /etc/pacman.conf
+sed -i '/\[multilib\]/{n;s/^#//g}' /etc/pacman.conf
+pacman -Syu --noconfirm
+
+# Installing drivers 
+pacman -S --noconfirm  nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia-prime lib32-mesa vulkan-intel lib32-vulkan-intel xf86-input-wacom xf86-input-libinput libva-intel-driver intel-media-driver
+
+
+# Installing services
+pacman -S --noconfirm  networkmanager openssh xdg-user-dirs haveged intel-ucode bluez bluez-libs
+
+# Enabling services
+systemctl enable NetworkManager haveged bluetooth
+
+# Installing sound libraries
+pacman -S --noconfirm  alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth
+
+# Installing filesystem libraries
+pacman -S --noconfirm  dosfstools ntfs-3g btrfs-progs exfat-utils gptfdisk autofs fuse2 fuse3 fuseiso sshfs
+
+# Installing compresion tools
+pacman -S --noconfirm  zip unzip unrar p7zip lzop pigz pbzip2
+
+# Installing generic tools
+pacman -S --noconfirm  vim nano pacman-contrib base-devel bash-completion usbutils lsof man net-tools inetutils
+
+# Installing paru
 newpass=$(< /dev/urandom tr -dc "@#*%&_A-Z-a-z-0-9" | head -c16)
 useradd -r -N -M -d /tmp/aurbuilder -s /usr/bin/nologin aurbuilder
 echo -e "$newpass\n$newpass\n" | passwd aurbuilder
@@ -40,53 +83,32 @@ chmod 777 /tmp/aurbuilder
 echo "aurbuilder ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/aurbuilder
 echo "root ALL=(aurbuilder) NOPASSWD: ALL" >> /etc/sudoers.d/aurbuilder
 cd /tmp/aurbuilder
-sudo -u aurbuilder git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-sudo -u aurbuilder makepkg -si
+sudo -u aurbuilder git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+sudo -u aurbuilder makepkg -si --noconfirm
 
-# Enabling colors in pacman
-sed -i "s/#Color/Color/g" /etc/pacman.conf
-
-# Enabling multilib repo
-sed -i '/\[multilib\]/s/^#//g' /etc/pacman.conf
-sed -i '/\[multilib\]/{n;s/^#//g}' /etc/pacman.conf
-pacman -Syu
-
-# Installing drivers 
-pacman -S --noconfirm nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia-prime lib32-mesa vulkan-intel lib32-vulkan-intel xf86-input-wacom xf86-input-libinput
-
-# Installing services
-pacman -S --noconfirm networkmanager openssh xdg-user-dirs haveged intel-ucode bluez bluez-libs
-
-# Enabling services
-systemctl enable NetworkManager haveged bluetooth
-
-# Installing sound libraries
-pacman -S --noconfirm alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth
-
-# Installing filesystem libraries
-pacman -S --noconfirm dosfstools ntfs-3g btrfs-progs exfat-utils gptfdisk autofs fuse2 fuse3 fuseiso sshfs
-
-# Installing compresion tools
-pacman -S --noconfirm zip unzip unrar p7zip lzop
-
-# Installing generic tools
-pacman -S --noconfirm vim nano pacman-contrib base-devel bash-completion usbutils lsof man net-tools inetutils
+# Optimizing aur
+cores=$(nproc)
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g" /etc/makepkg.conf
+sed -i "s/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q - --threads=0)/g" /etc/makepkg.conf
+sed -i "/^COMPRESSGZ/ s/gzip/pigz/g" /etc/makepkg.conf
+sed -i "/^COMPRESSBZ2/ s/bzip2/pbzip2/g" /etc/makepkg.conf
+sed -i "/^CFLAGS/ s/-march=x86-64 -mtune=generic/-march=native/g" /etc/makepkg.conf
+sed -i "/^CXXFLAGS/ s/-march=x86-64 -mtune=generic/-march=native/g" /etc/makepkg.conf
+sed -i "s/#RUSTFLAGS=\"-C opt-level=2\"/RUSTFLAGS=\"-C opt-level=2 -C target-cpu=native\"/g" /etc/makepkg.conf
 
 # Install XFCE
-pacman -S --noconfirm  xfce4 xfce4-goodies blueberry pavucontrol playerctl gvfs ffmpegthumbnailer network-manager-applet lightdm ttf-droid ttf-dejavu gtk-engine-murrine
+pacman -S --noconfirm xfce4 xfce4-goodies pavucontrol blueman playerctl xorg-xinput gvfs network-manager-applet lightdm gnome-mahjongg aisleriot ffmpegthumbnailer gtk-engine-murrine tilix transmission-gtk virt-manager geary webp-pixbuf-loader libgepub libgsf libopenraw materia-gtk-theme
+ 
+# Removing unwanted packages
+pacman -Rns --noconfirm parole xfburn
 
-# Installing lightdm-slick-greeter and lightdm-settings
-sudo -u aurbuilder yay -S lightdm-slick-greeter lightdm-settings plymouth
+# Installing plymouth
+sudo -u aurbuilder paru -S --noconfirm plymouth plymouth-theme-hexagon-2-git lightdm-slick-greeter lightdm-settings xfce-superkey-git
 
-# Change default lightdm greeter
+# Setting lightdm theme
 sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/g" /etc/lightdm/lightdm.conf
-
-# Enabling plymouth lightdm
-systemctl enable lightdm-plymouth
-
-# Installing hexagon_2 plymouth theme
-sudo -u aurbuilder yay -S plymouth-theme-hexagon-2-git
 
 # Making lone theme default
 plymouth-set-default-theme -R hexagon_2
@@ -97,71 +119,78 @@ sed -i "s/udev autodetect modconf block filesystems/udev plymouth autodetect mod
 sed -i "s/MODULES=()/MODULES=(i915)/g" /etc/mkinitcpio.conf
 mkinitcpio -P
 
-# Add kernel paramenters
-pacman -S --noconfirm --needed grub efibootmgr
-sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 cryptdevice=\/dev\/nvme0n1p2:luks:allow-discards root=\/dev\/lvm\/root intel_idle.max_cstate=1 apparmor=1 lsm=lockdown,yama,apparmor splash rd.udev.log_priority=3 vt.global_cursor_default=0"/' /etc/default/grub
-sed -i "s/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g" /etc/default/grub
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+# Install and configure systemd-boot
+pacman -S --noconfirm --needed efibootmgr
+bootctl install
+mkdir -p /boot/loader/entries
+cat > /boot/loader/loader.conf <<EOF
+default  arch.conf
+console-mode max
+editor   yes
+EOF
+cat > /boot/loader/entries/arch.conf <<EOF
+title   Arch Linux
+linux   /vmlinuz-linux-hardened
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-hardened.img
+options cryptdevice=/dev/disk/by-uuid/$(blkid -s UUID -o value /dev/nvme0n1p2):luks:allow-discards root=/dev/lvm/root apparmor=1 lsm=lockdown,yama,apparmor intel_idle.max_cstate=1 intel_iommu=igfx_off splash rd.udev.log_priority=3 vt.global_cursor_default=0 rw
+EOF
+cat > /boot/loader/entries/arch-fallback.conf <<EOF
+title   Arch Linux Fallback
+linux   /vmlinuz-linux-hardened
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-hardened-fallback.img
+options cryptdevice=/dev/disk/by-uuid/$(blkid -s UUID -o value /dev/nvme0n1p2):luks:allow-discards root=/dev/lvm/root apparmor=1 lsm=lockdown,yama,apparmor intel_idle.max_cstate=1 intel_iommu=igfx_off splash rd.udev.log_priority=3 vt.global_cursor_default=0 rw
+EOF
+bootctl update
 
-# Removing unwanted xfce apps
-pacman -Rnc xfburn parole
+# Enabling GDM
+systemctl enable lightdm-plymouth
 
 # Installing printing services
-pacman -S --noconfirm cups cups-pdf hplip ghostscript
+pacman -S --noconfirm  cups cups-pdf hplip ghostscript
 
 # Enabling cups service
 systemctl enable cups
 
 # Installing office utilities
-pacman -S --noconfirm libreoffice-fresh libreoffice-fresh-es hunspell-en_US hunspell-es_es mythes-en mythes-es hyphen-en hyphen-es
+pacman -S --noconfirm  libreoffice-fresh libreoffice-fresh-es hunspell-en_US hunspell-es_es mythes-en mythes-es hyphen-en hyphen-es
 
 # Installing multimedia codecs
-pacman -S --noconfirm gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav
+pacman -S --noconfirm  gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav
 
 # Installing gimp
-pacman -S --noconfirm gimp gimp-help-es
+pacman -S --noconfirm  gimp gimp-help-es
 
 # Installing required packages
-pacman -S --noconfirm tilix mpv strawberry jdk11-openjdk dolphin-emu discord telegram-desktop flatpak code wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald tlp earlyoom systembus-notify apparmor gamemode lib32-gamemode intel-undervolt firefox firefox-i18n-es-es chromium pepper-flash flashplugin transmission-gtk gparted noto-fonts font-bh-ttf gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse qemu libvirt nextcloud-client firewalld 
+pacman -S --noconfirm mpv jdk11-openjdk dolphin-emu discord telegram-desktop flatpak wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald earlyoom systembus-notify apparmor gamemode lib32-gamemode intel-undervolt firefox firefox-i18n-es-es gparted noto-fonts gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse qemu libvirt nextcloud-client firewalld obs-studio tlp neovim nodejs npm python-pynvim intellij-idea-community-edition libfido2 mednafen networkmanager-l2tp strongswan strawberry youtube-dl dbeaver pam-u2f
 
 # Enabling services
-systemctl enable thermald tlp earlyoom apparmor libvirtd firewalld
-
-# Optimizing aur
-sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
+systemctl enable thermald tlp earlyoom apparmor libvirtd firewalld 
 
 # Installing AUR packages
-sudo -u aurbuilder yay -S dxvk-bin aic94xx-firmware wd719x-firmware nerd-fonts-fantasque-sans-mono minecraft-launcher android-studio xampp qt5-styleplugins key-mapper-git mpv-mpris
-
-# Setting environment variable for QT5 theming
-echo "QT_QPA_PLATFORMTHEME=gtk2" | tee -a /etc/environment
-
-# Enable switcheroo-control and key-mapper
-systemctl enable key-mapper
-
-# Install plata-theme from git
-cd /home/link
-sudo -u link git clone https://aur.archlinux.org/plata-theme.git
-cd plata-theme
-sudo -u link sed -i 's/source=("git+https:\/\/gitlab.com\/tista500\/plata-theme.git#tag=${pkgver}")/source=("git+https:\/\/gitlab.com\/tista500\/plata-theme.git")/g' PKGBUILD
-clear
-echo "Installing plata-theme"
-sudo -u link makepkg -si
-cd ..
-rm -r plata-theme
-
-# Installing xorg for bug 865
 cd /tmp/aurbuilder
-git clone https://aur.archlinux.org/xorg-server-bug865.git
-cd xorg-server-bug865
-sed -i "s/''//" PKGBUILD
-sudo -u aurbuilder makepkg -si
+rm -r *
+for package in "dxvk-bin" "aic94xx-firmware" "wd719x-firmware" "nerd-fonts-cascadia-code" "mpv-mpris" "lbry-app-bin" "jdownloader2" "postman-bin" "mednaffe" "slack-desktop" "anydesk-bin" "visual-studio-code-bin" "google-chrome" "qt6gtk2"
+do
+	sudo -u aurbuilder git clone https://aur.archlinux.org/${package}.git
+	cd $package && sudo -u aurbuilder makepkg -si --noconfirm
+	cd ..
+	rm -r $package
+done
 
-# Install eclipse-jee with link's user
-clear
-echo "Installing eclipse-jee"
-sudo -u link yay -S eclipse-jee
+# Setting environment variable
+echo "QT_QPA_PLATFORMTHEME=qt5gtk2" | tee -a /etc/environment
+
+# Installing android studio
+sudo -u link paru -S --noconfirm android-studio 
+
+# Installing angular globally
+npm i -g @angular/cli
+ng analytics off
+
+# Installing ionic
+npm i -g @ionic/cli
 
 # Removing aurbuilder
 rm /etc/sudoers.d/aurbuilder
@@ -169,16 +198,21 @@ userdel aurbuilder
 rm -r /tmp/aurbuilder
 
 # Configuring intel-undervolt
-sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100/g" /etc/intel-undervolt.conf
-sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -100/g" /etc/intel-undervolt.conf
-sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -75/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -75/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -75/g" /etc/intel-undervolt.conf
 systemctl enable intel-undervolt
 
 # Adding flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Putting this option for the chrome-sandbox bullshit
+echo "kernel.unprivileged_userns_clone=1" | tee -a /etc/sysctl.d/99-sysctl.conf
+# echo "fs.inotify.max_user_watches=1048576" | tee -a /etc/sysctl.d/99-sysctl.conf
+echo "dev.i915.perf_stream_paranoid=0" | tee -a /etc/sysctl.d/99-sysctl.conf
+
 # Cleaning orphans
-pacman -Qtdq | pacman -Rns -
+pacman -Qtdq | pacman -Rns --noconfirm -
 
 # Copying dotfiles folder to link
 mv /dotfiles /home/link

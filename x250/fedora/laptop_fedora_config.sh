209637825ad56ad29bba1c5df8bf0fca85ac5e98
Alderaeney <alderaeney@alderaeney.com>
2021-05-15 10:18:37 +0200

    Removed xanmod kernel

--- x250/fedora/laptop_fedora_config.sh
+++ x250/fedora/laptop_fedora_config.sh
@@ -26,9 +26,6 @@ dnf copr enable pp3345/gnome-with-patches -y
 dnf install fedora-workstation-repositories -y
 dnf config-manager --set-enabled google-chrome
 
-#Enabling xanmod kernel repo
-dnf copr enable rmnscnce/kernel-xanmod -y
-
 #Enabling mednaffe repo
 dnf copr enable alderaeney/mednaffe -y
 
@@ -40,7 +37,7 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 dnf upgrade -y
 
 #Install required packages
-dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu fontconfig-enhanced-defaults fontconfig-font-replacements intel-undervolt ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 strawberry NetworkManager-l2tp-gnome google-chrome-stable mednafen mednaffe acpid youtube-dl kernel-xanmod-edge webp-pixbuf-loader pam-u2f pamu2fcfg libva-intel-hybrid-driver materia-kde materia-gtk-theme
+dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu fontconfig-enhanced-defaults fontconfig-font-replacements intel-undervolt ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 strawberry NetworkManager-l2tp-gnome google-chrome-stable mednafen mednaffe acpid youtube-dl webp-pixbuf-loader pam-u2f pamu2fcfg libva-intel-hybrid-driver materia-kde materia-gtk-theme
 
 systemctl enable thermald acpid 
 

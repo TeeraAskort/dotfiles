ff250e6a6240d6785bcc314ec2a800658cd8655d
Alderaeney <alderaeney@alderaeney.com>
2021-05-22 09:53:07 +0200

    Added home folder to config

--- desktop/home_folder.sh
+++ desktop/home_folder.sh
@@ -4,8 +4,31 @@ _script="$(readlink -f ${BASH_SOURCE[0]})"
 
 directory="$(dirname $_script)"
 
+dataDisk=$(lsblk -io KNAME,TYPE,MODEL | grep disk | grep TOSHIBA_DT01ACA300 | cut -d" " -f1)
+
 ## Adjusting keymap
 sudo localectl set-x11-keymap es
+
+## Configuring data disk
+sudo cryptsetup open /dev/${dataDisk}1 encrypteddata
+mkdir /home/link/Datos
+sudo mount /dev/mapper/encrypteddata /home/link/Datos
+sudo cp /home/link/Datos/.keyfile /root/.keyfile
+echo "encrypteddata UUID=$(sudo blkid -s UUID -o value /dev/${dataDisk}1) /root/.keyfile luks,discard" | sudo tee -a /etc/crypttab
+echo "/dev/mapper/encrypteddata /home/link/Datos btrfs defaults 0 0" | sudo tee -a /etc/fstab
+
+## Removing home folders
+rm -r ~/Descargas ~/Documentos ~/Escritorio ~/Música ~/Imágenes ~/Downloads ~/Torrent
+
+## Linking home folders
+ln -s /home/link/Datos/Descargas /home/link
+ln -s /home/link/Datos/Descargas /home/link/Downloads
+ln -s /home/link/Datos/Documentos /home/link
+ln -s /home/link/Datos/Escritorio /home/link
+ln -s /home/link/Datos/Música /home/link
+ln -s /home/link/Datos/Imágenes /home/link
+ln -s /home/link/Datos/Torrent /home/link
+ln -s /home/link/Datos/Nextcloud /home/link
 
 ## Installing vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -45,6 +68,12 @@ cp $directory/dotfiles/chromium-flags.conf ~/.config
 mkdir -p ~/.config/mpv/
 cp $directory/dotfiles/mpv.conf ~/.config/mpv/
 
+## Copy fonts
+mkdir ~/.fonts
+cd ~/.fonts
+unzip ~/Documentos/fonts.zip
+unzip ~/Documentos/fonts2.zip
+
 ## Changing GNOME theme
 if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
 	if [ -e /usr/share/themes/Materia-dark-compact ]; then
@@ -52,6 +81,7 @@ if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
 	else
 		gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 	fi
+	gsettings set org.gnome.desktop.interface monospace-font-name "Rec Mono Semicasual Regular 11"
 	gsettings set org.gnome.desktop.peripherals.mouse accel-profile "flat"
 	gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 	gsettings set org.gnome.desktop.privacy disable-camera true

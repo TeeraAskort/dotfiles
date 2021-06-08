af97ccd9765d32a7d54b9d53a72848dea86d129f
Alderaeney <alderaeney@alderaeney.com>
2021-06-08 09:48:23 +0200

    Added nixos 21.05 configuration

---
+++ gl63/nixos-21.05/materia-kde/default.nix
@@ -0,0 +1,28 @@
+ { stdenv, lib, fetchFromGitHub }:
+
+stdenv.mkDerivation rec {
+  pname = "materia-kde-theme";
+  version = "20210410";
+
+  src = fetchFromGitHub {
+    owner = "PapirusDevelopmentTeam";
+    repo = "materia-kde";
+    rev = version;
+    sha256 = "1ng16w0cbz857ps6cg36pm4564f65wn0r23ayv42sp2zm1x86cid";
+  };
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  # Make this a fixed-output derivation
+  outputHashMode = "recursive";
+  outputHashAlgo = "sha256";
+  ouputHash = "2c2def57092a399aa1c450699cbb8639f47d751157b18db17";
+
+  meta = {
+    description = "A port of the materia theme for Plasma";
+    homepage = "https://git.io/materia-kde";
+    license = lib.licenses.gpl3;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+}

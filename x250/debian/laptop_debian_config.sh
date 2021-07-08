9a9799bae1f37461477cc7603f81943add21e089
Alderaeney <alderaeney@alderaeney.com>
2021-07-08 22:39:23 +0200

    Removed aws

--- x250/debian/laptop_debian_config.sh
+++ x250/debian/laptop_debian_config.sh
@@ -111,7 +111,7 @@ if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ] || [ "$1" ==
 	flatpak install -y flathub com.discordapp.Discord io.lbry.lbry-app org.jdownloader.JDownloader org.DolphinEmu.dolphin-emu com.katawa_shoujo.KatawaShoujo com.google.AndroidStudio
 
 	# Installing angular globally
-	npm i -g @angular/cli @ionic/cli @vue/cli firebase-tools @aws-amplify/cli
+	npm i -g @angular/cli @ionic/cli @vue/cli firebase-tools 
 	ng analytics off
 
 	# Updating grub

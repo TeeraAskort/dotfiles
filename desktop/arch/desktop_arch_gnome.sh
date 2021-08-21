02d43a0766f51d3fc8bab505c06527d1295f2002
Alderaeney <alderaeney@alderaeney.com>
2021-08-21 15:34:28 +0200

    Added tilix to gnome installer

--- desktop/arch/desktop_arch_gnome.sh
+++ desktop/arch/desktop_arch_gnome.sh
@@ -102,7 +102,7 @@ sed -i "/^CXXFLAGS/ s/-march=x86-64 -mtune=generic/-march=native/g" /etc/makepkg
 sed -i "s/#RUSTFLAGS=\"-C opt-level=2\"/RUSTFLAGS=\"-C opt-level=2 -C target-cpu=native\"/g" /etc/makepkg.conf
 
 # Install GNOME
-pacman -S --noconfirm gnome gnome-tweaks gnome-nettool gnome-mahjongg aisleriot bubblewrap-suid ffmpegthumbnailer chrome-gnome-shell gtk-engine-murrine evolution gnome-boxes transmission-gtk gnome-software-packagekit-plugin webp-pixbuf-loader libgepub libgsf libopenraw materia-gtk-theme
+pacman -S --noconfirm gnome gnome-tweaks gnome-nettool gnome-mahjongg aisleriot bubblewrap-suid ffmpegthumbnailer chrome-gnome-shell gtk-engine-murrine evolution gnome-boxes transmission-gtk gnome-software-packagekit-plugin webp-pixbuf-loader libgepub libgsf libopenraw materia-gtk-theme tilix
  
 # Removing unwanted packages
 pacman -Rns --noconfirm gnome-music epiphany totem orca 

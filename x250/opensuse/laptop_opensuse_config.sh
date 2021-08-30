13e47f03aad209818e6add6057166a2bce9edca1
Alderaeney <alderaeney@alderaeney.com>
2021-08-30 16:02:23 +0200

    Added gpg key auto import

--- x250/opensuse/laptop_opensuse_config.sh
+++ x250/opensuse/laptop_opensuse_config.sh
@@ -20,7 +20,7 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
 
 # Refreshing the repos
-zypper refresh
+zypper refresh --gpg-auto-import-keys
 
 # Updating system
 zypper dup -y

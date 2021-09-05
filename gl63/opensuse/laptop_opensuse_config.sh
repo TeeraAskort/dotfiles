9a135c327fd2976fc38666c5f86fdae3a7f3f988
Alderaeney <alderaeney@alderaeney.com>
2021-09-05 12:49:19 +0200

    Fixed zypper packman

--- gl63/opensuse/laptop_opensuse_config.sh
+++ gl63/opensuse/laptop_opensuse_config.sh
@@ -30,13 +30,13 @@ zypper --gpg-auto-import-keys refresh
 zypper dup -y
 
 # Updating the system
-zypper dist-upgrade --from "Packman repository (openSUSE_Tumbleweed)" --allow-vendor-change -y
+zypper dist-upgrade --from packman --allow-vendor-change -y
 
 # Installing wine-staging from wine repo
 zypper in -y --from "Wine (openSUSE_Tumbleweed)" wine-staging wine-staging-32bit dxvk dxvk-32bit
 
 # Installing codecs
-zypper install -y --from "Packman repository (openSUSE_Tumbleweed)" ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full
+zypper install -y --from packman ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full
 
 # Installing basic packages
 zypper in -y chromium steam lutris papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions mpv mpv-mpris strawberry telegram-desktop flatpak gamemoded thermald plymouth-plugin-script nodejs npm intel-undervolt python39-neovim neovim noto-sans-cjk-fonts noto-coloremoji-fonts code earlyoom pam_u2f xf86-video-intel patterns-openSUSE-kvm_server patterns-server-kvm_tools qemu-audio-pa discord desmume zip dolphin-emu gimp flatpak-zsh-completion zsh-completions protontricks neofetch

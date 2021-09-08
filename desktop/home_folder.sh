69e28ba5cbc94482089f7e93836b96ec6a2b9588
Alderaeney <alderaeney@alderaeney.com>
2021-09-08 15:46:05 +0200

    Added xdg-override to desktop home_folder

--- desktop/home_folder.sh
+++ desktop/home_folder.sh
@@ -31,6 +31,13 @@ ln -s /home/link/Datos/Música /home/link
 ln -s /home/link/Datos/Imágenes /home/link
 ln -s /home/link/Datos/Nextcloud /home/link
 
+## Overriding xdg-user-dirs
+xdg-user-dirs-update --set DESKTOP $HOME/Datos/Escritorio
+xdg-user-dirs-update --set DOCUMENTS $HOME/Datos/Documentos
+xdg-user-dirs-update --set DOWNLOAD $HOME/Datos/Descargas
+xdg-user-dirs-update --set MUSIC $HOME/Datos/Música
+xdg-user-dirs-update --set PICTURES $HOME/Datos/Imágenes
+
 ## Configuring Torrent disk
 clear
 echo "Enter Torrent disk password:"

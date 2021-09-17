69d9e7f5616f0e1d2aa80018b543fb67ded0d31f
Alderaeney <alderaeney@alderaeney.com>
2021-09-17 19:25:12 +0200

    Updated debian and opensuse configs

--- desktop/opensuse/desktop_opensuse_config.sh
+++ desktop/opensuse/desktop_opensuse_config.sh
@@ -2,117 +2,122 @@
 
 if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
 
-rootDisk=$(lsblk -io KNAME,TYPE,MODEL | grep disk | grep WDC_WDS120G2G0B-00EPW0 | cut -d" " -f1)
+	rootDisk=$(lsblk -io KNAME,TYPE,MODEL | grep disk | grep WDC_WDS120G2G0B-00EPW0 | cut -d" " -f1)
 
-# Installing repos
-zypper ar http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/games.repo
-zypper ar http://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/Emulators:Wine.repo
-zypper ar http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/openSUSE_Tumbleweed/shells:zsh-users:zsh-syntax-highlighting.repo
-zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/openSUSE_Tumbleweed/shells:zsh-users:zsh-autosuggestions.repo
-zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/openSUSE_Tumbleweed/shells:zsh-users:zsh-completions.repo
-zypper ar https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/Emulators.repo
-zypper addrepo https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/hardware.repo
-zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman
-zypper addrepo https://download.opensuse.org/repositories/home:buschmann23/openSUSE_Tumbleweed/home:buschmann23.repo
-#  zypper ar https://repo.vivaldi.com/archive/vivaldi-suse.repo
+	# Installing repos
+	zypper ar http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/games.repo
+	zypper ar http://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/Emulators:Wine.repo
+	zypper ar http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/openSUSE_Tumbleweed/shells:zsh-users:zsh-syntax-highlighting.repo
+	zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/openSUSE_Tumbleweed/shells:zsh-users:zsh-autosuggestions.repo
+	zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/openSUSE_Tumbleweed/shells:zsh-users:zsh-completions.repo
+	zypper ar https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/Emulators.repo
+	zypper addrepo https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/hardware.repo
+	zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman
+	zypper addrepo https://download.opensuse.org/repositories/home:buschmann23/openSUSE_Tumbleweed/home:buschmann23.repo
+	zypper ar https://repo.vivaldi.com/archive/vivaldi-suse.repo
+	zypper addrepo https://download.opensuse.org/repositories/system:snappy/openSUSE_Tumbleweed/system:snappy.repo
 
-# Adding VSCode repo
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+	# Adding VSCode repo
+	rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
 
-# Refreshing the repos
-zypper --gpg-auto-import-keys refresh 
+	# Refreshing the repos
+	zypper --gpg-auto-import-keys refresh
 
-# Updating system
-zypper dup -y
+	# Updating system
+	zypper dup -y
 
-# Updating the system
-zypper dist-upgrade --from packman --allow-vendor-change -y
+	# Updating the system
+	zypper dist-upgrade --from packman --allow-vendor-change -y
 
-# Installing wine-staging from wine repo
-zypper in -y --from "Wine (openSUSE_Tumbleweed)" wine-staging wine-staging-32bit dxvk dxvk-32bit
+	# Installing wine-staging from wine repo
+	zypper in -y --from "Wine (openSUSE_Tumbleweed)" wine-staging wine-staging-32bit dxvk dxvk-32bit
 
-# Installing codecs
-zypper install -y --from packman --allow-vendor-change ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full
+	# Installing codecs
+	zypper install -y --from packman --allow-vendor-change ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full
 
-# Installing basic packages
-zypper in -y chromium steam lutris papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions mpv mpv-mpris strawberry flatpak gamemoded thermald plymouth-plugin-script nodejs npm python39-neovim neovim noto-sans-cjk-fonts noto-coloremoji-fonts earlyoom discord code patterns-openSUSE-kvm_server patterns-server-kvm_tools qemu-audio-pa desmume zip dolphin-emu gimp flatpak-zsh-completion zsh-completions protontricks neofetch 
+	# Installing basic packages
+	zypper in -y chromium steam lutris papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions mpv mpv-mpris strawberry flatpak gamemoded thermald plymouth-plugin-script nodejs npm python39-neovim neovim noto-sans-cjk-fonts noto-coloremoji-fonts earlyoom discord code patterns-openSUSE-kvm_server patterns-server-kvm_tools qemu-audio-pa desmume zip dolphin-emu gimp flatpak-zsh-completion zsh-completions protontricks neofetch vivaldi-stable php8 snapd virtualbox filezilla
 
-# Enabling thermald service
-systemctl enable thermald earlyoom libvirtd
+	# Enabling thermald service
+	systemctl enable thermald earlyoom libvirtd
 
-# Removing unwanted applications
-zypper rm -y git-gui vlc vlc-qt vlc-noX
+	# Removing unwanted applications
+	zypper rm -y git-gui vlc vlc-qt vlc-noX
 
-# Block vlc from installing
-zypper addlock vlc-beta
-zypper addlock vlc
+	# Block vlc from installing
+	zypper addlock vlc-beta
+	zypper addlock vlc
 
-if [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
-	# Installing DE specific applications
-	zypper in -y yakuake qbittorrent kdeconnect-kde palapeli gnome-keyring pam_kwallet gnome-keyring-pam k3b kio_audiocd MozillaThunderbird
+	if [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
+		# Installing DE specific applications
+		zypper in -y yakuake qbittorrent kdeconnect-kde palapeli gnome-keyring pam_kwallet gnome-keyring-pam k3b kio_audiocd MozillaThunderbird
 
-	# Removing unwanted DE specific applications
-	zypper rm -y konversation kmines ksudoku kreversi
+		# Removing unwanted DE specific applications
+		zypper rm -y konversation kmines ksudoku kreversi
 
-	# Adding GTK_USE_PORTAL
-	echo "GTK_USE_PORTAL=1" | tee -a /etc/environment
+		# Adding GTK_USE_PORTAL
+		echo "GTK_USE_PORTAL=1" | tee -a /etc/environment
 
-	# Adding gnome-keyring settings
-	cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-	if diff /etc/pam.d/sddm.bak sddm ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-		cp sddm /etc/pam.d/sddm
-	else
-		sudo cp sddm /etc/pam.d/sddm
+		# Adding gnome-keyring settings
+		cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
+		awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+		if diff /etc/pam.d/sddm.bak sddm; then
+			awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+			cp sddm /etc/pam.d/sddm
+		else
+			sudo cp sddm /etc/pam.d/sddm
+		fi
+		rm sddm
+		cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
+		awk "FNR==NR{ if (/session /) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+		if diff /etc/pam.d/sddm.bak sddm; then
+			awk "FNR==NR{ if (/session\t/) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+			cp sddm /etc/pam.d/sddm
+		else
+			sudo cp sddm /etc/pam.d/sddm
+		fi
+		rm sddm
 	fi
-	rm sddm
-	cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
-	awk "FNR==NR{ if (/session /) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-	if diff /etc/pam.d/sddm.bak sddm ; then
-		awk "FNR==NR{ if (/session\t/) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-		cp sddm /etc/pam.d/sddm
-	else
-		sudo cp sddm /etc/pam.d/sddm
-	fi
-	rm sddm
-fi
 
-# Changing plymouth theme
-until wget https://github.com/adi1090x/files/raw/master/plymouth-themes/themes/pack_2/hexagon_2.tar.gz
-do
-	echo "Download failed, retrying"
-done
-tar xzvf hexagon_2.tar.gz
-mv hexagon_2 /usr/share/plymouth/themes/
-plymouth-set-default-theme -R hexagon_2
-rm hexagon_2.tar.gz
+	# Changing plymouth theme
+	until wget https://github.com/adi1090x/files/raw/master/plymouth-themes/themes/pack_2/hexagon_2.tar.gz; do
+		echo "Download failed, retrying"
+	done
+	tar xzvf hexagon_2.tar.gz
+	mv hexagon_2 /usr/share/plymouth/themes/
+	plymouth-set-default-theme -R hexagon_2
+	rm hexagon_2.tar.gz
 
-# Removing double encryption password asking
-touch /.root.key
-chmod 600 /.root.key
-dd if=/dev/urandom of=/.root.key bs=1024 count=1
-clear
-echo "Enter disk encryption password"
-until  cryptsetup luksAddKey /dev/${rootDisk}2 /.root.key
-do
-	echo "Retrying"
-done
-sed -i "/WDC_WDS120G2G0B-00EPW0/ s/none/\/.root.key/g" /etc/crypttab
-echo -e 'install_items+=" /.root.key "' |  tee --append /etc/dracut.conf.d/99-root-key.conf >/dev/null
-echo "/boot/ root:root 700" |  tee -a /etc/permissions.local
-chkstat --system --set
-mkinitrd
+	# Removing double encryption password asking
+	touch /.root.key
+	chmod 600 /.root.key
+	dd if=/dev/urandom of=/.root.key bs=1024 count=1
+	clear
+	echo "Enter disk encryption password"
+	until cryptsetup luksAddKey /dev/${rootDisk}2 /.root.key; do
+		echo "Retrying"
+	done
+	sed -i "/WDC_WDS120G2G0B-00EPW0/ s/none/\/.root.key/g" /etc/crypttab
+	echo -e 'install_items+=" /.root.key "' | tee --append /etc/dracut.conf.d/99-root-key.conf >/dev/null
+	echo "/boot/ root:root 700" | tee -a /etc/permissions.local
+	chkstat --system --set
+	mkinitrd
 
-# Adding flathub repo
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	# Adding flathub repo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Installing flatpak apps
-flatpak install -y flathub io.lbry.lbry-app org.jdownloader.JDownloader com.google.AndroidStudio com.jetbrains.IntelliJ-IDEA-Community org.eclipse.Java com.mojang.Minecraft com.getpostman.Postman com.github.AmatCoder.mednaffe org.telegram.desktop io.dbeaver.DBeaverCommunity
+	# Installing flatpak apps
+	flatpak install -y flathub io.lbry.lbry-app org.jdownloader.JDownloader com.google.AndroidStudio com.jetbrains.IntelliJ-IDEA-Community org.eclipse.Java com.mojang.Minecraft com.getpostman.Postman com.github.AmatCoder.mednaffe org.telegram.desktop io.dbeaver.DBeaverCommunity com.axosoft.GitKraken
 
-# Flatpak overrides
-flatpak override --filesystem=~/.fonts
+	# Flatpak overrides
+	flatpak override --filesystem=~/.fonts
+
+	# Installing xampp
+	curl -L "https://www.apachefriends.org/xampp-files/8.0.10/xampp-linux-x64-8.0.10-0-installer.run" >xampp.run
+	chmod 755 xampp.run
+	./xampp.run --unattendedmodeui minimal --mode unattended
+	rm xampp.run
 
 else
 	echo "Accepted paramenters:"
--- gl63/debian/laptop_debian_config.sh
+++ gl63/debian/laptop_debian_config.sh
@@ -14,96 +14,104 @@ if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ] || [ "$1" ==
 	# Installing curl
 	apt install -y curl wget software-properties-common
 
+	# Adding backports and fasttrack repos
+	echo "deb http://deb.debian.org/debian bullseye-backports main contrib non-free" | tee -a /etc/apt/sources.list
+	apt update
+	apt install fasttrack-archive-keyring
+	echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib" | tee -a /etc/apt/sources.list
+	echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib" | tee -a /etc/apt/sources.list
+	apt update
+
 	# Installing drivers
 	apt install -y linux-headers-amd64 nvidia-driver firmware-misc-nonfree libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 firmware-linux-nonfree
 
 	# Adding vivaldi repo
-	# wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | apt-key add -
-	# add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' 
-	# apt update
-       	# apt install -y vivaldi-stable
+	wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | apt-key add -
+	add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
+	apt update
+	apt install -y vivaldi-stable
 	# apt remove -y firefox-esr
-	
+
 	# Installing strawberry
-	curl -s https://api.github.com/repos/strawberrymusicplayer/strawberry/releases/latest \
-	| grep "browser_download_url" \
-	| grep "strawberry_" \
-	| grep "bullseye" \
-	| cut -d '"' -f 4 \
-	| wget -O strawberry.deb -qi -
+	curl -s https://api.github.com/repos/strawberrymusicplayer/strawberry/releases/latest |
+		grep "browser_download_url" |
+		grep "strawberry_" |
+		grep "bullseye" |
+		cut -d '"' -f 4 |
+		wget -O strawberry.deb -qi -
 	apt install -y ./strawberry.deb
-	
+
 	# Installing wine
 	wget -nc https://dl.winehq.org/wine-builds/winehq.key
 	apt-key add winehq.key
 	echo "deb https://dl.winehq.org/wine-builds/debian/ bullseye main" | tee /etc/apt/sources.list.d/wine.list
 	apt update
-       	apt install -y winehq-staging winetricks
+	apt install -y winehq-staging winetricks
 
 	# Installing VSCode
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
 	install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list
 	rm -f packages.microsoft.gpg
 	apt install apt-transport-https && apt update
 	apt install -y code
-	
+
 	# Add deb-multimedia repo
 	echo "deb https://www.deb-multimedia.org bullseye main non-free" | tee /etc/apt/sources.list.d/multimedia.list
 	apt-get update -oAcquire::AllowInsecureRepositories=true
 	apt install deb-multimedia-keyring --allow-unauthenticated
 	apt update
-       	apt full-upgrade -y
+	apt full-upgrade -y
 
 	# Installing lutris
 	echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | tee /etc/apt/sources.list.d/lutris.list
 	wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
 	apt update
-       	apt install -y lutris
+	apt install -y lutris
 
 	# Installing required applications
-	apt install -y build-essential steam vim nano fonts-noto fonts-noto-cjk fonts-noto-mono mednafen mednaffe telegram-desktop neovim python3-neovim gimp flatpak papirus-icon-theme zsh zsh-autosuggestions zsh-syntax-highlighting thermald mpv youtube-dl chromium libreoffice firmware-linux libfido2-1 gamemode hyphen-en-us mythes-en-us btrfs-progs gparted ntfs-3g exfat-utils f2fs-tools unrar hplip printer-driver-cups-pdf earlyoom obs-studio gstreamer1.0-vaapi desmume openjdk-11-jdk pamu2fcfg libpam-u2f zip unzip nodejs npm 
+	apt install -y build-essential steam vim nano fonts-noto fonts-noto-cjk fonts-noto-mono mednafen mednaffe telegram-desktop neovim python3-neovim gimp flatpak papirus-icon-theme zsh zsh-autosuggestions zsh-syntax-highlighting thermald mpv youtube-dl chromium libreoffice firmware-linux libfido2-1 gamemode hyphen-en-us mythes-en-us btrfs-progs gparted ntfs-3g exfat-utils f2fs-tools unrar hplip printer-driver-cups-pdf earlyoom obs-studio gstreamer1.0-vaapi desmume openjdk-11-jdk pamu2fcfg libpam-u2f zip unzip nodejs npm php snapd filezilla virtualbox virtualbox-ext-pack
 
 	systemctl enable thermald
-	
+
 	if [ "$1" == "gnome" ]; then
 		# Installing required packages
-		apt install materia-gtk-theme qt5-qmake qtbase5-private-dev libgtk2.0-0 libx11-6 ffmpegthumbnailer tilix transmission-gtk evolution aisleriot gnome-mahjongg 
-		
+		apt install materia-gtk-theme qt5-qmake qtbase5-private-dev libgtk2.0-0 libx11-6 ffmpegthumbnailer tilix transmission-gtk evolution aisleriot gnome-mahjongg
+
 		# Installing qt5gtk2
 		git clone https://bitbucket.org/trialuser02/qt5gtk2.git
 		cd qt5gtk2
 		qmake && make && make install
 		cd .. && rm -r qt5gtk2
 		echo "QT_QPA_PLATFORMTHEME=qt5gtk2" | tee -a /etc/environment
-		
+
 		# Remove unwanted applications
-		apt remove -y totem rhythmbox 
-		
+		apt remove -y totem rhythmbox
+
 	elif [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
 		# Installing required packages
 		apt install -y qbittorrent palapeli kmahjongg kpat thunderbird thunderbird-l10n-es-es yakuake gnome-keyring libpam-gnome-keyring libpam-kwallet5 sddm-theme-breeze kdeconnect plasma-browser-integration qemu-system libvirt-clients libvirt-daemon-system virt-manager xdg-desktop-portal-kde ffmpegthumbs kde-config-tablet dolphin-plugins k3b kio-audiocd libreoffice-qt5 libreoffice-kf5 xdg-desktop-portal
-		
+
 		# Adding user to libvirt group
 		adduser $user libvirt
-		
+
 		# Remove unwanted applications
 		apt remove -y konversation akregator kmail konqueror dragonplayer juk kaddressbook korganizer xdg-desktop-portal-gtk
 
 		# Adding environment variable
 		echo "GTK_USE_PORTAL=1" | tee -a /etc/environment
-		
+
 	elif [ "$1" == "xfce" ]; then
 		# Installing required packages
 		apt install -y tilix gvfs gvfs-backends thunderbird materia-gtk-theme qt5-qmake qtbase5-private-dev libgtk2.0-0 libx11-6 ffmpegthumbnailer tumbler tumbler-plugins-extra transmission-gtk
-		
+
 		# Installing qt5gtk2
 		git clone https://bitbucket.org/trialuser02/qt5gtk2.git
 		cd qt5gtk2
 		qmake && make && make install
 		cd .. && rm -r qt5gtk2
 		echo "QT_QPA_PLATFORMTHEME=qt5gtk2" | tee -a /etc/environment
-	
+
 	elif [ "$1" == "cinnamon" ]; then
 		# Installing required packages
 		apt install -y tilix gvfs gvfs-backends materia-gtk-theme materia-kde qt5-style-kvantum transmission-gtk aisleriot gnome-mahjongg ffmpegthumbnailer lightdm-settings slick-greeter xdg-desktop-portal-gtk libpam-gnome-keyring libgepub-0.6-0 libgsf-1-114 libwebp6 libopenraw7 geary
@@ -115,15 +123,15 @@ if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ] || [ "$1" ==
 		echo "QT_STYLE_OVERRIDE=kvantum" | tee -a /etc/environment
 
 	fi
-	
+
 	# Removing unused packages
 	apt autoremove -y
-	
+
 	#Add flathub repo
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 	#Install flatpak applications
-	flatpak install -y flathub com.discordapp.Discord io.lbry.lbry-app org.jdownloader.JDownloader org.DolphinEmu.dolphin-emu com.google.AndroidStudio 
+	flatpak install -y flathub com.discordapp.Discord io.lbry.lbry-app org.jdownloader.JDownloader org.DolphinEmu.dolphin-emu com.google.AndroidStudio com.getpostman.Postman org.eclipse.Java io.dbeaver.DBeaverCommunity com.axosoft.GitKraken com.jetbrains.IntelliJ-IDEA-Community
 
 	# Updating grub
 	sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 splash"/' /etc/default/grub
@@ -143,10 +151,15 @@ if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ] || [ "$1" ==
 	# Putting sysctl options
 	echo "dev.i915.perf_stream_paranoid=0" | tee -a /etc/sysctl.d/99-sysctl.conf
 
+	# Installing xampp
+	curl -L "https://www.apachefriends.org/xampp-files/8.0.10/xampp-linux-x64-8.0.10-0-installer.run" >xampp.run
+	chmod 755 xampp.run
+	./xampp.run --unattendedmodeui minimal --mode unattended
+	rm xampp.run
+
 else
 	echo "Accepted paramenters:"
 	echo "kde or plasma - to configure the plasma desktop"
 	echo "gnome - to configure the GNOME desktop"
 	echo "xfce - to configure the XFCE desktop"
 fi
-

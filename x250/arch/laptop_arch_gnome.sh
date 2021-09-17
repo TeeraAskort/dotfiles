9e7a5fe741670faf1b2b309bb66325750d41d5eb
Alderaeney <alderaeney@alderaeney.com>
2021-09-17 21:25:15 +0200

    Changed postman for insomnia

--- x250/arch/laptop_arch_gnome.sh
+++ x250/arch/laptop_arch_gnome.sh
@@ -175,7 +175,7 @@ pacman -S --needed --noconfirm wine-staging giflib lib32-giflib libpng lib32-lib
 # Installing AUR packages
 cd /tmp/aurbuilder
 rm -r *
-for package in "dxvk-bin" "aic94xx-firmware" "wd719x-firmware" "mpv-mpris" "lbry-app-bin" "jdownloader2" "visual-studio-code-bin" "qt6gtk2" "chrome-gnome-shell" "pfetch" "youtube-dlp-bin" "yt-dlp-drop-in" "postman-bin" "minecraft-launcher" "gitkraken" "vivaldi-widevine"
+for package in "dxvk-bin" "aic94xx-firmware" "wd719x-firmware" "mpv-mpris" "lbry-app-bin" "jdownloader2" "visual-studio-code-bin" "qt6gtk2" "chrome-gnome-shell" "pfetch" "youtube-dlp-bin" "yt-dlp-drop-in" "insomnia-bin" "minecraft-launcher" "gitkraken" "vivaldi-widevine"
 do
 	sudo -u aurbuilder git clone https://aur.archlinux.org/${package}.git
 	cd $package && sudo -u aurbuilder makepkg -si --noconfirm

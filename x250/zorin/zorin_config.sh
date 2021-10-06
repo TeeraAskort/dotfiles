0e8ad4fac07927242dcd2ef9c2a13330d5d80f11
Alderaeney <alderaeney@alderaeney.com>
2021-10-06 16:56:15 +0200

    Fixed mongo installation

--- x250/zorin/zorin_config.sh
+++ x250/zorin/zorin_config.sh
@@ -41,6 +41,7 @@ wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt update
 apt install -y mongodb-org
+systemctl restart mongod.service
 mongosh farmcrash $directory/mongodb.js
 
 # Disabling youtube-dl installation

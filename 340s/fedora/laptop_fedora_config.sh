7d259062720ac28da9ad368485062aca85832e24
Alderaeney <alderaeney@gmail.com>
2021-10-19 14:27:34 +0200

    Added opensuse config

---
+++ 340s/fedora/laptop_fedora_config.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+
+directory="$(dirname $_script)"
+
+user=$SUDO_USER
+
+#DNF Tweaks
+echo "deltarpm=true" | tee -a /etc/dnf/dnf.conf
+echo "max_parallel_downloads=10" | tee -a /etc/dnf/dnf.conf 
+
+#Setting up hostname
+hostnamectl set-hostname link-x250
+
+#Install RPMfusion
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+#Better font rendering cpor
+dnf copr enable dawid/better_fonts -y
+
+#Enabling mednaffe repo
+dnf copr enable alderaeney/mednaffe -y
+
+#Enabling vivaldi repo
+# dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
+
+#Install VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+
+# Upgrade system
+dnf upgrade -y --refresh
+
+# Wine dependencies
+dnf install -y alsa-plugins-pulseaudio.i686 glibc-devel.i686 glibc-devel libgcc.i686 libX11-devel.i686 freetype-devel.i686 libXcursor-devel.i686 libXi-devel.i686 libXext-devel.i686 libXxf86vm-devel.i686 libXrandr-devel.i686 libXinerama-devel.i686 mesa-libGLU-devel.i686 mesa-libOSMesa-devel.i686 libXrender-devel.i686 libpcap-devel.i686 ncurses-devel.i686 libzip-devel.i686 lcms2-devel.i686 zlib-devel.i686 libv4l-devel.i686 libgphoto2-devel.i686 cups-devel.i686 libxml2-devel.i686 openldap-devel.i686 libxslt-devel.i686 gnutls-devel.i686 libpng-devel.i686 flac-libs.i686 json-c.i686 libICE.i686 libSM.i686 libXtst.i686 libasyncns.i686 liberation-narrow-fonts.noarch libieee1284.i686 libogg.i686 libsndfile.i686 libuuid.i686 libva.i686 libvorbis.i686 libwayland-client.i686 libwayland-server.i686 llvm-libs.i686 mesa-dri-drivers.i686 mesa-filesystem.i686 mesa-libEGL.i686 mesa-libgbm.i686 nss-mdns.i686 ocl-icd.i686 pulseaudio-libs.i686 sane-backends-libs.i686 tcp_wrappers-libs.i686 unixODBC.i686 samba-common-tools.x86_64 samba-libs.x86_64 samba-winbind.x86_64 samba-winbind-clients.x86_64 samba-winbind-modules.x86_64 mesa-libGL-devel.i686 fontconfig-devel.i686 libXcomposite-devel.i686 libtiff-devel.i686 openal-soft-devel.i686 mesa-libOpenCL-devel.i686 opencl-utils-devel.i686 alsa-lib-devel.i686 gsm-devel.i686 libjpeg-turbo-devel.i686 pulseaudio-libs-devel.i686 pulseaudio-libs-devel gtk3-devel.i686 libattr-devel.i686 libva-devel.i686 libexif-devel.i686 libexif.i686 glib2-devel.i686 mpg123-devel.i686 mpg123-devel.x86_64 libcom_err-devel.i686 libcom_err-devel.x86_64 libFAudio-devel.i686 libFAudio-devel.x86_64
+
+sudo dnf groupinstall "C Development Tools and Libraries" -y
+sudo dnf groupinstall "Development Tools" -y
+
+#Install required packages
+dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu fontconfig-enhanced-defaults fontconfig-font-replacements ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 clementine chromium-freeworld mednafen mednaffe webp-pixbuf-loader brasero desmume unrar gimp mpv-mpris protontricks libnsl mod_perl java-11-openjdk-devel ffmpeg rtmpdump aria2 AtomicParsley dkms elfutils-libelf-devel qt5-qtx11extras VirtualBox gtk-murrine-engine gtk2-engines kernel-headers kernel-devel
+
+systemctl enable thermald 
+
+# Adding user to vboxusers group
+user="$SUDO_USER"
+usermod -aG vboxusers $user 
+
+# Installing computer specific packages
+dnf in -y pam-u2f pamu2fcfg libva-intel-hybrid-driver intel-undervolt tlp
+
+# Enabling services
+systemctl enable intel-undervolt tlp
+
+# Remove unused packages 
+dnf remove -y totem rhythmbox 
+
+#Update Appstream data
+dnf groupupdate core -y
+
+#Install multimedia codecs
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y --allowerasing
+dnf groupupdate sound-and-video -y
+
+#Disable wayland
+sed -i "s/#WaylandEnable=false/WaylandEnable=false/" /etc/gdm/custom.conf 
+
+#Intel undervolt configuration
+sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -75/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -75/g" /etc/intel-undervolt.conf
+sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -75/g" /etc/intel-undervolt.conf
+
+systemctl enable intel-undervolt
+
+#Installing qogir theme
+curl -L "https://api.github.com/repos/vinceliuice/Qogir-theme/tarball" > Qogir-gtk.tar.gz
+tar xzvf Qogir-gtk.tar.gz && cd *Qogir-theme*
+./install.sh -l fedora -c dark -w square
+cd .. && rm -r *Qogir*
+git clone https://github.com/vinceliuice/Qogir-kde.git
+cd Qogir-kde
+./install.sh
+cd .. && rm -r Qogir-kde
+
+#Add flathub repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#Install flatpak applications
+flatpak install -y flathub com.discordapp.Discord io.lbry.lbry-app com.google.AndroidStudio org.jdownloader.JDownloader org.telegram.desktop org.eclipse.Java com.axosoft.GitKraken com.mojang.Minecraft com.getpostman.Postman io.dbeaver.DBeaverCommunity
+
+# Flatpak overrides
+flatpak override --filesystem=~/.fonts
+
+# Add sysctl config
+echo "dev.i915.perf_stream_paranoid=0" | tee -a /etc/sysctl.d/99-sysctl.conf
+
+# Installing xampp
+ver="8.0.11"
+until curl -L "https://www.apachefriends.org/xampp-files/${ver}/xampp-linux-x64-${ver}-0-installer.run" > xampp.run; do
+	echo "Retrying"
+done
+chmod 755 xampp.run
+./xampp.run --unattendedmodeui minimal --mode unattended
+rm xampp.run
+
+# Setting hostname properly for xampp
+echo "127.0.0.1    $(hostname)" | tee -a /etc/hosts
+
+# Installing yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
+ln -s /usr/bin/yt-dlp /usr/bin/youtube-dl
---
+++ 340s/opensuse/laptop_opensuse_config.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+
+directory="$(dirname $_script)"
+
+if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
+
+	# Installing repos
+	zypper ar http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/games.repo
+	zypper ar http://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/Emulators:Wine.repo
+	zypper ar http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/openSUSE_Tumbleweed/shells:zsh-users:zsh-syntax-highlighting.repo
+	zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/openSUSE_Tumbleweed/shells:zsh-users:zsh-autosuggestions.repo
+	zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/openSUSE_Tumbleweed/shells:zsh-users:zsh-completions.repo
+	zypper ar https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/Emulators.repo
+	zypper addrepo https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/hardware.repo
+	zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/' packman
+	zypper addrepo https://download.opensuse.org/repositories/home:buschmann23/openSUSE_Tumbleweed/home:buschmann23.repo
+	zypper addrepo https://download.opensuse.org/repositories/games:tools/openSUSE_Tumbleweed/games:tools.repo
+	zypper addrepo https://download.opensuse.org/repositories/mozilla/openSUSE_Tumbleweed/mozilla.repo
+	# zypper ar https://repo.vivaldi.com/archive/vivaldi-suse.repo
+
+	# Adding VSCode repo
+	rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+
+	# Refreshing the repos
+	zypper --gpg-auto-import-keys refresh
+
+	# Updating system
+	zypper dup -y
+
+	# Updating the system
+	zypper dist-upgrade --from packman --allow-vendor-change -y
+
+	# Installing wine-staging from wine repo
+	zypper in -y --from "Wine (openSUSE_Tumbleweed)" wine-staging wine-staging-32bit dxvk dxvk-32bit
+
+	# Installing codecs
+	zypper install -y --from packman --allow-vendor-change ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full
+
+	# Installing discord from games:tools repo
+	zypper in -y --from 'Tools for Gamers (openSUSE_Tumbleweed)' --allow-vendor-change discord gamemoded
+
+	# Installing basic packages
+	zypper in -y chromium steam lutris papirus-icon-theme vim zsh zsh-syntax-highlighting zsh-autosuggestions mpv mpv-mpris clementine flatpak thermald plymouth-plugin-script nodejs npm intel-undervolt python39-neovim neovim noto-sans-cjk-fonts noto-coloremoji-fonts code earlyoom xf86-video-intel desmume zip dolphin-emu gimp flatpak-zsh-completion zsh-completions protontricks neofetch php8 virtualbox filezilla net-tools net-tools-deprecated net-tools-lang php-composer2 minecraft-launcher virtualbox-host-source kernel-devel kernel-default-devel mariadb mariadb-client
+
+	# Enabling thermald service
+	systemctl enable thermald intel-undervolt earlyoom mariadb
+
+	# Starting services
+	systemctl start mariadb
+
+	# Removing unwanted applications
+	zypper rm -y git-gui vlc vlc-qt vlc-noX
+
+	# Block vlc from installing
+	zypper addlock vlc-beta
+	zypper addlock vlc
+
+	if [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
+		# Installing DE specific applications
+		zypper in -y yakuake qbittorrent kdeconnect-kde palapeli gnome-keyring pam_kwallet gnome-keyring-pam k3b kio_audiocd MozillaThunderbird
+
+		# Removing unwanted DE specific applications
+		zypper rm -y konversation kmines ksudoku kreversi
+
+		# Adding GTK_USE_PORTAL
+		echo "GTK_USE_PORTAL=1" | tee -a /etc/environment
+
+		# Adding gnome-keyring settings
+		cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
+		awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+		if diff /etc/pam.d/sddm.bak sddm; then
+			awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth     optional       pam_gnome_keyring.so\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+			cp sddm /etc/pam.d/sddm
+		else
+			sudo cp sddm /etc/pam.d/sddm
+		fi
+		rm sddm
+		cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
+		awk "FNR==NR{ if (/session /) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+		if diff /etc/pam.d/sddm.bak sddm; then
+			awk "FNR==NR{ if (/session\t/) p=NR; next} 1; FNR==p{ print \"session  optional       pam_gnome_keyring.so auto_start\" }" /etc/pam.d/sddm /etc/pam.d/sddm >sddm
+			cp sddm /etc/pam.d/sddm
+		else
+			sudo cp sddm /etc/pam.d/sddm
+		fi
+		rm sddm
+
+		# Adding gnome-keyring to passwd pam setings
+		echo "password	optional	pam_gnome_keyring.so" | tee -a /etc/pam.d/passwd
+	fi
+
+	# Installing firefox from mozilla repo
+	zypper dup --from "Mozilla based projects (openSUSE_Tumbleweed)" --allow-vendor-change -y
+
+	# Changing plymouth theme
+	until wget https://github.com/adi1090x/files/raw/master/plymouth-themes/themes/pack_4/rings.tar.gz; do
+		echo "Download failed, retrying"
+	done
+	tar xzvf rings.tar.gz
+	mv rings /usr/share/plymouth/themes/
+	plymouth-set-default-theme -R rings
+	rm rings.tar.gz
+
+	# Removing double encryption password asking
+	touch /.root.key
+	chmod 600 /.root.key
+	dd if=/dev/urandom of=/.root.key bs=1024 count=1
+	echo ""
+	echo "Enter disk encryption password"
+	until cryptsetup luksAddKey /dev/nvme0n1p2 /.root.key; do
+		echo "Retrying"
+	done
+	sed -i "/WDC_WDS500G2B0C/ s/none/\/.root.key/g" /etc/crypttab
+	echo -e 'install_items+=" /.root.key "' | tee --append /etc/dracut.conf.d/99-root-key.conf >/dev/null
+	echo "/boot/ root:root 700" | tee -a /etc/permissions.local
+	chkstat --system --set
+	mkinitrd
+
+	#Intel undervolt configuration
+	sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -75/g" /etc/intel-undervolt.conf
+	sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -75/g" /etc/intel-undervolt.conf
+	sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -75/g" /etc/intel-undervolt.conf
+
+	# Adding flathub repo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+	# Installing flatpak apps
+	flatpak install -y flathub io.lbry.lbry-app org.jdownloader.JDownloader com.google.AndroidStudio org.eclipse.Java com.github.AmatCoder.mednaffe org.telegram.desktop com.axosoft.GitKraken com.getpostman.Postman io.dbeaver.DBeaverCommunity
+
+	# Installing flatpak themes
+	if [ "$1" == "kde" ] || [ "$1" == "plasma" ]; then
+		flatpak install -y flathub org.gtk.Gtk3theme.Breeze-Dark org.gtk.Gtk3theme.Breeze
+	fi
+
+	# Flatpak overrides
+	flatpak override --filesystem=~/.fonts
+
+	# Add sysctl config
+	echo "dev.i915.perf_stream_paranoid=0" | tee -a /etc/sysctl.d/99-sysctl.conf
+
+	# Installing xampp
+	ver="8.0.11"
+	until curl -L "https://www.apachefriends.org/xampp-files/${ver}/xampp-linux-x64-${ver}-0-installer.run" > xampp.run; do
+		echo "Retrying"
+	done
+	chmod 755 xampp.run
+	./xampp.run --unattendedmodeui minimal --mode unattended
+	rm xampp.run
+
+	# Setting hostname properly for xampp
+	echo "127.0.0.1    $(hostname)" | tee -a /etc/hosts
+
+	# Adding user to vboxusers group
+	user="$SUDO_USER"
+	usermod -aG vboxusers $user 
+
+else
+	echo "Accepted paramenters:"
+	echo "kde or plasma - to configure the plasma desktop"
+	echo "gnome - to configure the GNOME desktop"
+fi

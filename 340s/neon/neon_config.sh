3e36380b8cc866bcca6935f13b714ddb35eefb7c
Alderaeney <alderaeney@gmail.com>
2021-10-28 20:01:55 +0200

    Updated neon config

--- 340s/neon/neon_config.sh
+++ 340s/neon/neon_config.sh
@@ -16,6 +16,8 @@ add-apt-repository ppa:kisak/kisak-mesa -y
 add-apt-repository ppa:ondrej/php -y
 add-apt-repository ppa:serge-rider/dbeaver-ce -y
 add-apt-repository ppa:papirus/papirus -y
+add-apt-repository ppa:mc3man/mpv-tests -y
+add-apt-repository ppa:jonaski/strawberry -y
 
 # Installing wine
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
@@ -46,30 +48,26 @@ echo "virtualbox-ext-pack virtualbox-ext-pack/license select true" | debconf-set
 echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
 
 # Installing required packages
-apt install -y clementine mpv vim neovim python3-neovim zsh zsh-autosuggestions zsh-syntax-highlighting flatpak curl wget thermald earlyoom gamemode build-essential xz-utils openjdk-11-jdk net-tools fonts-noto-cjk libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 apt-transport-https hunspell-es hunspell-en-us aspell-es aspell-en mythes-es mythes-en-us net-tools fonts-noto-color-emoji libfido2-1 libglu1-mesa mednafen mednaffe ffmpeg zip unzip unrar python3-mutagen rtmpdump phantomjs php8.0 composer hplip virtualbox virtualbox-dkms virtualbox-ext-pack lutris desmume filezilla printer-driver-cups-pdf f2fs-tools btrfs-progs exfat-fuse dbeaver-ce mariadb-server mariadb-client ttf-mscorefonts-installer libasound2-dev tilix cryptsetup libreoffice libreoffice-qt5 gnome-keyring palapeli kpat qbittorrent thunderbird thunderbird-locale-es-es yakuake ffmpegthumbs okular-backends okular-extra-backends kdeconnect k3b k3b-i18n kio-audiocd kio-extras kio-smtp kio-gdrive papirus-icon-theme kate 
+apt install -y strawberry mpv vim neovim python3-neovim zsh zsh-autosuggestions zsh-syntax-highlighting flatpak curl wget thermald earlyoom gamemode build-essential xz-utils openjdk-11-jdk net-tools fonts-noto-cjk libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 apt-transport-https hunspell-es hunspell-en-us aspell-es aspell-en mythes-es mythes-en-us net-tools fonts-noto-color-emoji libfido2-1 libglu1-mesa mednafen mednaffe ffmpeg zip unzip unrar python3-mutagen rtmpdump phantomjs php8.0 composer hplip virtualbox virtualbox-dkms virtualbox-ext-pack lutris desmume filezilla printer-driver-cups-pdf f2fs-tools btrfs-progs exfat-fuse dbeaver-ce mariadb-server mariadb-client ttf-mscorefonts-installer libasound2-dev tilix cryptsetup libreoffice libreoffice-qt5 gnome-keyring palapeli kpat qbittorrent thunderbird thunderbird-locale-es-es yakuake ffmpegthumbs okular-backends okular-extra-backends kdeconnect k3b k3b-i18n kio-audiocd kio-extras kio-smtp kio-gdrive papirus-icon-theme kate 
 
 # Install computer specific packages
-apt install -y intel-media-va-driver tlp libvulkan1 libvulkan1:i386 libpam-fprintd libpam-u2f pamu2fcfg
+apt install -y intel-media-va-driver libvulkan1 libvulkan1:i386 libpam-fprintd libpam-u2f pamu2fcfg
 
 # Removing unwanted packages
 apt remove -y vlc
 
 # Installing mpv-mpris
-apt install -y libmpv-dev libglib2.0-dev
-git clone https://github.com/hoyon/mpv-mpris.git
-cd mpv-mpris
-make
-mkdir /etc/mpv/scripts
-cp mpris.so /etc/mpv/scripts
-cd .. && rm -r mpv-mpris
-apt remove -y libmpv-dev libglib2.0-dev
+curl -LO "https://github.com/hoyon/mpv-mpris/releases/latest/download/mpris.so"
+mkdir -p /etc/mpv/scripts
+mv mpris.so /etc/mpv/scripts/mpris.so
 
 # Installing outsider applications
 curl -LO "https://cdn.akamai.steamstatic.com/client/installer/steam.deb"
 curl -LO "https://launcher.mojang.com/download/Minecraft.deb"
 curl -L "https://github.com/lbryio/lbry-desktop/releases/download/v0.51.2/LBRY_0.51.2.deb?_ga=2.242496066.1377799971.1633284702-265872098.1633284702" > lbry.deb
-apt install -y ./steam.deb ./Minecraft.deb ./lbry.deb
-rm steam.deb Minecraft.deb lbry.deb
+curl -L "https://release.gitkraken.com/linux/gitkraken-amd64.deb" > gitkraken.deb
+apt install -y ./steam.deb ./Minecraft.deb ./lbry.deb ./gitkraken.deb
+rm steam.deb Minecraft.deb lbry.deb gitkraken.deb
 
 # Adding GTK_USE_PORTAL
 echo "GTK_USE_PORTAL=1" | tee -a /etc/environment
@@ -109,12 +107,20 @@ apt install code -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Installing flatpak applications
-flatpak install -y flathub org.eclipse.Java com.axosoft.GitKraken org.jdownloader.JDownloader com.getpostman.Postman org.chromium.Chromium com.google.AndroidStudio org.telegram.desktop com.discordapp.Discord org.gtk.Gtk3theme.Breeze-Dark
+flatpak install -y flathub org.jdownloader.JDownloader com.getpostman.Postman org.telegram.desktop com.discordapp.Discord com.jetbrains.PhpStorm org.gtk.Gtk3theme.Breeze-Dark org.gtk.Gtk3theme.Breeze
+user="$SUDO_USER"
+sudo -u $user flatpak override --user --filesystem=/home/$user/.local/share/color-schemes
 
 # Installing yt-dlp
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 chmod a+rx /usr/local/bin/yt-dlp
 ln -s /usr/local/bin/yt-dlp /usr/bin/youtube-dl
+
+# Installing chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee -a /etc/apt/sources.list.d/google-chrome.list
+apt update
+apt install -y google-chrome-stable
 
 # Adding resume var to grub
 part=$(blkid | grep swap | cut -d":" -f1)
@@ -131,7 +137,7 @@ cp $directory/../common/hibernate-kde.pkla /etc/polkit-1/localauthority/50-local
 # echo "HibernateState=disk" | tee -a /etc/systemd/sleep.conf
 
 # Installing xampp
-ver="8.0.11"
+ver="8.0.12"
 until curl -L "https://www.apachefriends.org/xampp-files/${ver}/xampp-linux-x64-${ver}-0-installer.run" > xampp.run; do
 	echo "Retrying"
 done
@@ -141,6 +147,12 @@ rm xampp.run
 
 # Setting hostname properly for xampp
 echo "127.0.0.1    $(hostname)" | tee -a /etc/hosts
+
+# Installing eclipse
+curl -L "https://rhlx01.hs-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2021-09/R/eclipse-jee-2021-09-R-linux-gtk-x86_64.tar.gz" > eclipse-jee.tar.gz
+tar xzvf eclipse-jee.tar.gz -C /opt
+rm eclipse-jee.tar.gz
+desktop-file-install $directory/../common/eclipse.desktop
 
 # Removing uneeded packages
 apt autoremove --purge -y

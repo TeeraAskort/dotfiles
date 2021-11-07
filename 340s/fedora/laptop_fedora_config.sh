9cef5b37838026b965c500aca4e6c7ad6315febc
Alderaeney <alderaeney@gmail.com>
2021-11-07 19:30:00 +0100

    Added tainted repos install

--- 340s/fedora/laptop_fedora_config.sh
+++ 340s/fedora/laptop_fedora_config.sh
@@ -16,6 +16,9 @@ hostnamectl set-hostname link-340s
 #Install RPMfusion
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
+#Installing tainted repos
+dnf in -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+
 #Enabling mednaffe repo
 dnf copr enable alderaeney/mednaffe -y
 

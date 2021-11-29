9b00144275d08695ea1cc3ed24cef09a256e10e5
Alderaeney <alderaeney@gmail.com>
2021-11-29 19:00:36 +0100

    Updated gl63 config

--- gl63/fedora-centos/laptop_fedora_config.sh
+++ gl63/fedora-centos/laptop_fedora_config.sh
@@ -16,14 +16,20 @@ hostnamectl set-hostname link-gl63-8rc
 #Install RPMfusion
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-#Better font rendering cpor
-dnf copr enable dawid/better_fonts -y
+#Installing tainted repos
+dnf in -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
 
 #Enabling mednaffe repo
 dnf copr enable alderaeney/mednaffe -y
 
-#Enabling lxc repo
-dnf copr enable ganto/lxc4 -y
+# Enabling touchegg repo
+dnf copr enable jose_exposito/touchegg -y
+
+# Enabling better_fonts repo
+dnf copr enable aldrich/better_fonts -y
+
+# Adding docker repo
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 #Enabling negativo17 nvidia repo
 # dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
@@ -39,25 +45,33 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 dnf upgrade -y --refresh
 
 # Wine dependencies
-sudo dnf install -y alsa-plugins-pulseaudio.i686 glibc-devel.i686 glibc-devel libgcc.i686 libX11-devel.i686 freetype-devel.i686 libXcursor-devel.i686 libXi-devel.i686 libXext-devel.i686 libXxf86vm-devel.i686 libXrandr-devel.i686 libXinerama-devel.i686 mesa-libGLU-devel.i686 mesa-libOSMesa-devel.i686 libXrender-devel.i686 libpcap-devel.i686 ncurses-devel.i686 libzip-devel.i686 lcms2-devel.i686 zlib-devel.i686 libv4l-devel.i686 libgphoto2-devel.i686 cups-devel.i686 libxml2-devel.i686 openldap-devel.i686 libxslt-devel.i686 gnutls-devel.i686 libpng-devel.i686 flac-libs.i686 json-c.i686 libICE.i686 libSM.i686 libXtst.i686 libasyncns.i686 liberation-narrow-fonts.noarch libieee1284.i686 libogg.i686 libsndfile.i686 libuuid.i686 libva.i686 libvorbis.i686 libwayland-client.i686 libwayland-server.i686 llvm-libs.i686 mesa-dri-drivers.i686 mesa-filesystem.i686 mesa-libEGL.i686 mesa-libgbm.i686 nss-mdns.i686 ocl-icd.i686 pulseaudio-libs.i686 sane-backends-libs.i686 tcp_wrappers-libs.i686 unixODBC.i686 samba-common-tools.x86_64 samba-libs.x86_64 samba-winbind.x86_64 samba-winbind-clients.x86_64 samba-winbind-modules.x86_64 mesa-libGL-devel.i686 fontconfig-devel.i686 libXcomposite-devel.i686 libtiff-devel.i686 openal-soft-devel.i686 mesa-libOpenCL-devel.i686 opencl-utils-devel.i686 alsa-lib-devel.i686 gsm-devel.i686 libjpeg-turbo-devel.i686 pulseaudio-libs-devel.i686 pulseaudio-libs-devel gtk3-devel.i686 libattr-devel.i686 libva-devel.i686 libexif-devel.i686 libexif.i686 glib2-devel.i686 mpg123-devel.i686 mpg123-devel.x86_64 libcom_err-devel.i686 libcom_err-devel.x86_64 libFAudio-devel.i686 libFAudio-devel.x86_64
+dnf install -y alsa-plugins-pulseaudio.i686 glibc-devel.i686 glibc-devel libgcc.i686 libX11-devel.i686 freetype-devel.i686 libXcursor-devel.i686 libXi-devel.i686 libXext-devel.i686 libXxf86vm-devel.i686 libXrandr-devel.i686 libXinerama-devel.i686 mesa-libGLU-devel.i686 mesa-libOSMesa-devel.i686 libXrender-devel.i686 libpcap-devel.i686 ncurses-devel.i686 libzip-devel.i686 lcms2-devel.i686 zlib-devel.i686 libv4l-devel.i686 libgphoto2-devel.i686 cups-devel.i686 libxml2-devel.i686 openldap-devel.i686 libxslt-devel.i686 gnutls-devel.i686 libpng-devel.i686 flac-libs.i686 json-c.i686 libICE.i686 libSM.i686 libXtst.i686 libasyncns.i686 liberation-narrow-fonts.noarch libieee1284.i686 libogg.i686 libsndfile.i686 libuuid.i686 libva.i686 libvorbis.i686 libwayland-client.i686 libwayland-server.i686 llvm-libs.i686 mesa-dri-drivers.i686 mesa-filesystem.i686 mesa-libEGL.i686 mesa-libgbm.i686 nss-mdns.i686 ocl-icd.i686 pulseaudio-libs.i686 sane-backends-libs.i686 tcp_wrappers-libs.i686 unixODBC.i686 samba-common-tools.x86_64 samba-libs.x86_64 samba-winbind.x86_64 samba-winbind-clients.x86_64 samba-winbind-modules.x86_64 mesa-libGL-devel.i686 fontconfig-devel.i686 libXcomposite-devel.i686 libtiff-devel.i686 openal-soft-devel.i686 mesa-libOpenCL-devel.i686 opencl-utils-devel.i686 alsa-lib-devel.i686 gsm-devel.i686 libjpeg-turbo-devel.i686 pulseaudio-libs-devel.i686 pulseaudio-libs-devel gtk3-devel.i686 libattr-devel.i686 libva-devel.i686 libexif-devel.i686 libexif.i686 glib2-devel.i686 mpg123-devel.i686 mpg123-devel.x86_64 libcom_err-devel.i686 libcom_err-devel.x86_64 libFAudio-devel.i686 libFAudio-devel.x86_64
 
-sudo dnf groupinstall "C Development Tools and Libraries" -y
-sudo dnf groupinstall "Development Tools" -y
+dnf groupinstall "C Development Tools and Libraries" -y
+dnf groupinstall "Development Tools" -y
 
 #Install required packages
-dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu fontconfig-enhanced-defaults fontconfig-font-replacements ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 clementine chromium-freeworld mednafen mednaffe webp-pixbuf-loader brasero desmume unrar gimp mpv-mpris protontricks libnsl mod_perl java-11-openjdk-devel lxd lxc ffmpeg rtmpdump aria2 AtomicParsley dkms elfutils-libelf-devel qt5-qtx11extras VirtualBox gtk-murrine-engine gtk2-engines kernel-headers kernel-devel
+dnf install -y vim lutris steam mpv flatpak zsh zsh-syntax-highlighting papirus-icon-theme transmission-gtk wine winetricks gnome-tweaks dolphin-emu ffmpegthumbnailer zsh-autosuggestions google-noto-cjk-fonts google-noto-emoji-color-fonts google-noto-emoji-fonts nodejs npm code aisleriot thermald gnome-mahjongg evolution python-neovim libfido2 strawberry chromium-freeworld mednafen mednaffe webp-pixbuf-loader brasero desmume unrar gimp mpv-mpris protontricks libnsl mod_perl java-11-openjdk-devel ffmpeg dkms elfutils-libelf-devel qt5-qtx11extras VirtualBox gtk-murrine-engine gtk2-engines kernel-headers kernel-devel discord pcsx2 neofetch unzip zip cryptsetup alsa-plugins-pulseaudio.x86_64 alsa-lib-devel.x86_64 nicotine+ file-roller composer docker-ce docker-ce-cli containerd.io docker-compose yt-dlp minigalaxy obs-studio fontconfig-font-replacements fontconfig-enhanced-defaults
 
-systemctl enable thermald
+# Enabling services
+systemctl enable thermald docker
+
+# Starting services
+systemctl start docker
 
 # Adding user to vboxusers group
 user="$SUDO_USER"
 usermod -aG vboxusers $user 
 
 # Installing computer specific packages
-dnf in -y intel-undervolt libva-intel-hybrid-driver pam-u2f pamu2fcfg tlp
+dnf in -y intel-undervolt libva-intel-hybrid-driver pam-u2f pamu2fcfg tlp touchegg
 
 # Enabling services
 systemctl enable intel-undervolt
+
+# Adding user to docker group
+user="$SUDO_USER"
+usermod -aG docker $user
 
 # Remove unused packages 
 dnf remove -y totem rhythmbox 
@@ -66,8 +80,11 @@ dnf remove -y totem rhythmbox
 dnf groupupdate core -y
 
 #Install multimedia codecs
-dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y --allowerasing
 dnf groupupdate sound-and-video -y
+dnf install -y libdvdcss
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+dnf install -y lame\* --exclude=lame-devel
+dnf group upgrade -y --with-optional Multimedia
 
 #Install nvidia drivers
 dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
@@ -89,18 +106,6 @@ sed -i "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100/g" /etc/intel-undervolt.con
 sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -100/g" /etc/intel-undervolt.conf
 sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
 
-systemctl enable intel-undervolt
-
-#Installing qogir theme
-curl -L "https://api.github.com/repos/vinceliuice/Qogir-theme/tarball" > Qogir-gtk.tar.gz
-tar xzvf Qogir-gtk.tar.gz && cd *Qogir-theme*
-./install.sh -l fedora -c dark -w square
-cd .. && rm -r *Qogir*
-git clone https://github.com/vinceliuice/Qogir-kde.git
-cd Qogir-kde
-./install.sh
-cd .. && rm -r Qogir-kde
-
 # Changing tlp config
 sed -i "s/#CPU_ENERGY_PERF_POLICY_ON_AC=balance_performance/CPU_ENERGY_PERF_POLICY_ON_AC=balance_power/g" /etc/tlp.conf
 sed -i "s/#SCHED_POWERSAVE_ON_AC=0/SCHED_POWERSAVE_ON_AC=1/g" /etc/tlp.conf
@@ -111,29 +116,19 @@ systemctl enable tlp
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Install flatpak applications
-flatpak install -y flathub com.discordapp.Discord io.lbry.lbry-app com.google.AndroidStudio org.jdownloader.JDownloader org.telegram.desktop org.eclipse.Java com.axosoft.GitKraken com.mojang.Minecraft com.getpostman.Postman io.dbeaver.DBeaverCommunity
+flatpak install -y flathub io.lbry.lbry-app org.jdownloader.JDownloader org.telegram.desktop com.axosoft.GitKraken com.getpostman.Postman io.dbeaver.DBeaverCommunity org.gtk.Gtk3theme.Adwaita-dark com.jetbrains.PhpStorm com.google.AndroidStudio io.gdevs.GDLauncher
 
 # Flatpak overrides
 flatpak override --filesystem=~/.fonts
 
-# Add sysctl config
-echo "dev.i915.perf_stream_paranoid=0" | tee -a /etc/sysctl.d/99-sysctl.conf
-
-# Installing xampp
-until curl -L "https://www.apachefriends.org/xampp-files/8.0.10/xampp-linux-x64-8.0.10-0-installer.run" > xampp.run; do
-	echo "Retrying"
-done
-chmod 755 xampp.run
-./xampp.run --unattendedmodeui minimal --mode unattended
-rm xampp.run
-
-# Setting hostname properly for xampp
-echo "127.0.0.1    $(hostname)" | tee -a /etc/hosts
-
 # Installing yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-chmod a+rx /usr/local/bin/yt-dlp
 ln -s /usr/bin/yt-dlp /usr/bin/youtube-dl
+
+# Installing eclipse
+curl -L "https://rhlx01.hs-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2021-09/R/eclipse-jee-2021-09-R-linux-gtk-x86_64.tar.gz" > eclipse-jee.tar.gz
+tar xzvf eclipse-jee.tar.gz -C /opt
+rm eclipse-jee.tar.gz
+desktop-file-install $directory/../../common/eclipse.desktop
 
 # Add intel_idle.max_cstate=1 to grub and update
 grubby --update-kernel=ALL --args='intel_idle.max_cstate=1'
--- gl63/home_folder.sh
+++ gl63/home_folder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 _script="$(readlink -f ${BASH_SOURCE[0]})"
 
@@ -9,15 +9,20 @@ user=$USER
 ## Adjusting keymap
 sudo localectl set-x11-keymap es
 
-sudo cryptsetup open /dev/sda1 encrypteddata
+## Configuring data disk
+until sudo cryptsetup open /dev/sda1 encrypteddata; do
+	echo "Bad password, retrying"
+done
 mkdir $HOME/Datos
-sudo mount /dev/mapper/encrypteddata /home/link/Datos 
+sudo mount /dev/mapper/encrypteddata $HOME/Datos 
 sudo cp $HOME/Datos/.keyfile /root/.keyfile
 echo "encrypteddata UUID=784ab40a-1cce-4d15-8df2-1cfe9f9dfa5b /root/.keyfile luks,discard" | sudo tee -a /etc/crypttab
 echo "/dev/mapper/encrypteddata $HOME/Datos btrfs defaults 0 0" | sudo tee -a /etc/fstab
 
+## Removing home folders
 rm -r ~/Descargas ~/Documentos ~/Escritorio ~/Música ~/Imágenes ~/Downloads ~/Torrent
 
+## Linking home folders
 ln -s $HOME/Datos/Descargas $HOME
 ln -s $HOME/Datos/Descargas $HOME/Downloads
 ln -s $HOME/Datos/Documentos $HOME
@@ -27,6 +32,7 @@ ln -s $HOME/Datos/Imágenes $HOME
 ln -s $HOME/Datos/Torrent $HOME
 ln -s $HOME/Datos/Nextcloud $HOME
 
+## Overriding xdg-user-dirs
 xdg-user-dirs-update --set DESKTOP $HOME/Datos/Escritorio
 xdg-user-dirs-update --set DOCUMENTS $HOME/Datos/Documentos
 xdg-user-dirs-update --set DOWNLOAD $HOME/Datos/Descargas
@@ -42,16 +48,19 @@ sudo sed -i "s/undervolt 1 'GPU' 0/undervolt 1 'GPU' -100/g" /etc/intel-undervol
 sudo sed -i "s/undervolt 2 'CPU Cache' 0/undervolt 2 'CPU Cache' -100/g" /etc/intel-undervolt.conf
 sudo systemctl enable intel-undervolt
 
+## Installing vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-       
+
+## Installing Ohmyzsh and powerlevel10k 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
+## Copying dotfiles
 cp $directory/zsh/.zshrc ~
 cp $directory/zsh/.general_alias ~
 cp $directory/zsh/.arch_alias ~
@@ -61,14 +70,10 @@ cp $directory/zsh/.silverblue_alias ~
 cp $directory/zsh/.opensuse_alias ~
 cp $directory/zsh/.elementary_alias ~
 cp $directory/zsh/.solus_alias ~
-cp $directory/zsh/.zorin_alias ~
+cp $directory/zsh/.ubuntu_alias ~
 mkdir -p ~/.config/pulse
 cp $directory/dotfiles/daemon.conf ~/.config/pulse/
 pulseaudio -k
-pipewireS=$(systemctl --user list-unit-files --type service | grep pipewire.service | wc -l)
-if [ $pipewireS -eq 1 ]; then 
-	cp -r $directory/../common/pipewire /etc
-fi
 
 # Copying .zshenv on debian
 if [ $(lsb_release -is | grep "Debian" | wc -l) -eq 1 ]; then
@@ -91,6 +96,7 @@ curl -LO https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637
 mv SSimDownscaler.glsl KrigBilateral.glsl ~/.config/mpv/shaders
 cp $directory/dotfiles/mpv.conf ~/.config/mpv/
 
+## Copy fonts
 mkdir ~/.fonts
 cd ~/.fonts
 unzip ~/Documentos/fonts.zip
@@ -99,9 +105,9 @@ unzip ~/Documentos/fonts2.zip
 # Installing NPM packages
 if command -v rpm-ostree &> /dev/null; then
 	npm config set prefix '~/.node_packages'
-	npm install -g @angular/cli @vue/cli
+	npm install -g @angular/cli @vue/cli @ionic/cli sass
 else
-	sudo npm install -g @angular/cli @vue/cli
+	sudo npm install -g @angular/cli @vue/cli @ionic/cli sass
 fi
 
 # Enabling opentabletdriver service
@@ -114,13 +120,18 @@ else
 	sudo udevadm control --reload-rules
 fi
 
+## Configuring docker
+cd $directory/../common
+sudo systemctl restart docker
+sudo docker-compose -f compose.yml up -d --build
 
-# Configuring mariadb
-if command -v mysql &> /dev/null ; then
-	sudo mysql -u root -e "CREATE DATABASE farmcrash"
-	sudo mysql -u root -e "CREATE USER 'farmcrash'@localhost IDENTIFIED BY 'farmcrash'"
-	sudo mysql -u root -e "GRANT ALL PRIVILEGES ON farmcrash.* TO 'farmcrash'@localhost IDENTIFIED BY 'farmcrash'"
-fi
+## Copying ssh key
+mkdir ~/.ssh
+cp ~/Documentos/id_ed25519* ~/.ssh
+eval `ssh-agent -s`
+until ssh-add ~/.ssh/id_ed25519; do
+	echo "Bad password, retrying"
+done
 
 ## Configuring u2f cards
 hostnm=$(hostname)
@@ -133,84 +144,14 @@ pamu2fcfg -o pam://"$hostnm" -i pam://"$hostnm" > ~/.config/Yubico/u2f_keys
 echo "Remove FIDO2 car and insert another, then press a key:"
 read -n 1
 pamu2fcfg -o pam://"$hostnm" -i pam://"$hostnm" -n >> ~/.config/Yubico/u2f_keys
-sudo sed -i "2i auth            sufficient      pam_u2f.so origin=pam://$hostnm appid=pam://$hostnm cue" /etc/pam.d/sudo
-sudo sed -i "/auth.*substack.*system-auth/a auth\tsufficient\tpam_u2f.so cue origin=pam://$hostnm appid=pam://$hostnm cue" /etc/pam.d/su
-if [ -e /etc/pam.d/gdm-password ]; then
-	sudo cp /etc/pam.d/gdm-password /etc/pam.d/gdm-password.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/gdm-password /etc/pam.d/gdm-password > gdm-password
-	if diff /etc/pam.d/gdm-password.bak gdm-password ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/gdm-password /etc/pam.d/gdm-password > gdm-password
-		sudo cp gdm-password /etc/pam.d/gdm-password
-	else
-		sudo cp gdm-password /etc/pam.d/gdm-password
-	fi
-	rm gdm-password
-fi
 
-if [ -e /etc/pam.d/xfce4-screensaver ]; then
-	sudo cp /etc/pam.d/xfce4-screensaver /etc/pam.d/xfce4-screensaver.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/xfce4-screensaver /etc/pam.d/xfce4-screensaver > xfce4-screensaver
-	if diff /etc/pam.d/xfce4-screensaver.bak xfce4-screensaver ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/xfce4-screensaver /etc/pam.d/xfce4-screensaver > xfce4-screensaver
-		sudo cp xfce4-screensaver /etc/pam.d/xfce4-screensaver
-	else
-		sudo cp xfce4-screensaver /etc/pam.d/xfce4-screensaver
-	fi
-	rm xfce4-screensaver
-fi
-
-if [ -e /etc/pam.d/lightdm ]; then
-	sudo cp /etc/pam.d/lightdm /etc/pam.d/lightdm.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/lightdm /etc/pam.d/lightdm > lightdm
-	if diff /etc/pam.d/lightdm.bak lightdm ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/lightdm /etc/pam.d/lightdm > lightdm
-		sudo cp lightdm /etc/pam.d/lightdm
-	else
-		sudo cp lightdm /etc/pam.d/lightdm
-	fi
-	rm lightdm
-fi
-
-if [ -e /etc/pam.d/sddm ]; then
-	sudo cp /etc/pam.d/sddm /etc/pam.d/sddm.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-	if diff /etc/pam.d/sddm.bak sddm ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/sddm /etc/pam.d/sddm > sddm
-		sudo cp sddm /etc/pam.d/sddm
-	else
-		sudo cp sddm /etc/pam.d/sddm
-	fi
-	rm sddm
-fi
-
-if [ -e /etc/pam.d/kde ]; then
-	sudo cp /etc/pam.d/kde /etc/pam.d/kde.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/kde /etc/pam.d/kde > kde
-	if diff /etc/pam.d/kde.bak kde ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/kde /etc/pam.d/kde > kde
-		sudo cp kde /etc/pam.d/kde
-	else
-		sudo cp kde /etc/pam.d/kde
-	fi
-	rm kde
-fi
-
-if [ -e /etc/pam.d/polkit-1 ]; then
-	sudo cp /etc/pam.d/polkit-1 /etc/pam.d/polkit-1.bak
-	awk "FNR==NR{ if (/auth /) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/polkit-1 /etc/pam.d/polkit-1 > polkit-1
-	if diff /etc/pam.d/polkit-1.bak polkit-1 ; then
-		awk "FNR==NR{ if (/auth\t/) p=NR; next} 1; FNR==p{ print \"auth            required      pam_u2f.so nouserok origin=pam://$hostnm appid=pam://$hostnm\" }" /etc/pam.d/polkit-1 /etc/pam.d/polkit-1 > polkit-1
-		sudo cp polkit-1 /etc/pam.d/polkit-1
-	else
-		sudo cp polkit-1 /etc/pam.d/polkit-1
-	fi
-	rm polkit-1
-fi
+bash $directory/pam_config.sh
 
 ## Changing GNOME theme
 if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
 	if [ -e /usr/share/themes/Materia-dark-compact ]; then
 		gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
+
 	elif [ -e /usr/share/themes/Qogir-win-dark ]; then
 		gsettings set org.gnome.desktop.interface gtk-theme "Qogir-win-dark"
 	else
@@ -233,11 +174,12 @@ if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
 	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 900
 	gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
 	gsettings set org.gnome.nautilus.icon-view default-zoom-level 'small'
-	gsettings set org.gnome.desktop.peripherals.trackball accel-profile 'flat'
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3700
 	gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-	gsettings set org.gnome.settings-daemon.plugins.power lid-close-suspend-with-external-monitor true
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power power-button-action hibernate
 	if [ -e /usr/share/icons/Papirus-Dark/ ]; then
 		gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
 	fi
@@ -259,11 +201,48 @@ if [[ "$XDG_CURRENT_DESKTOP" == "zorin:GNOME" ]]; then
 	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
 	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 900
 	gsettings set org.gnome.nautilus.icon-view default-zoom-level 'small'
-	gsettings set org.gnome.desktop.peripherals.trackball accel-profile 'flat'
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3700
 	gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+	gsettings set org.gnome.settings-daemon.plugins.power button-power hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action hibernate
 	gsettings set org.gnome.settings-daemon.plugins.power lid-close-suspend-with-external-monitor true
+	gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power power-button-action hibernate
+fi
+
+## Changing Budgie config
+if [[ "$XDG_CURRENT_DESKTOP" == "Budgie:GNOME" ]]; then
+	if [ -e /usr/share/themes/Plata-Noir-Compact ]; then
+		gsettings set org.gnome.desktop.interface gtk-theme "Plata-Noir-Compact"
+	fi
+	gsettings set org.gnome.desktop.interface monospace-font-name "Rec Mono Semicasual Regular 11"
+	gsettings set org.gnome.desktop.peripherals.mouse accel-profile "flat"
+	gsettings set org.gnome.desktop.privacy disable-camera true
+	gsettings set org.gnome.desktop.privacy disable-microphone true
+	gsettings set org.gnome.desktop.privacy remember-recent-files false
+	gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+	gsettings set org.gnome.desktop.privacy remove-old-trash-files true
+	gsettings set org.gnome.desktop.privacy old-files-age 3
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 900
+	gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+	gsettings set org.gnome.nautilus.icon-view default-zoom-level 'small'
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3700
+	gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+	gsettings set org.gnome.settings-daemon.plugins.power button-power hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power lid-close-suspend-with-external-monitor true
+	gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action hibernate
+	gsettings set org.gnome.settings-daemon.plugins.power power-button-action hibernate
+	if [ -e /usr/share/icons/Papirus-Dark/ ]; then
+		gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+	fi
 fi
 
 # XFCE config
@@ -280,6 +259,7 @@ git config --global init.defaultBranch master
 git config --global credential.helper store
 
 ## Adding user to audio group
+user="$USER"
 sudo usermod -aG audio $user
 
 ## Changing user shell

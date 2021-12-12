983c8e273b6cd3fdf11fca4e69856aed9cb8a777
Alderaeney <alderaeney@gmail.com>
2021-12-12 11:19:14 +0100

    Added phpstorm and docker

--- 340s/arch/desktop_install.sh
+++ 340s/arch/desktop_install.sh
@@ -208,10 +208,13 @@ pacman -S --noconfirm gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plu
 pacman -S --noconfirm gimp gimp-help-es
 
 # Installing required packages
-pacman -S --noconfirm mpv jdk11-openjdk dolphin-emu discord telegram-desktop flatpak wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald earlyoom systembus-notify apparmor gamemode lib32-gamemode firefox firefox-i18n-es-es gparted noto-fonts gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse firewalld obs-studio neovim nodejs npm python-pynvim libfido2 strawberry pam-u2f yad mednafen virtualbox virtualbox-host-dkms filezilla php chromium composer dbeaver nicotine+ yt-dlp
+pacman -S --noconfirm mpv jdk11-openjdk dolphin-emu discord telegram-desktop flatpak wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald earlyoom systembus-notify apparmor gamemode lib32-gamemode firefox firefox-i18n-es-es gparted noto-fonts gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse firewalld obs-studio neovim nodejs npm python-pynvim libfido2 strawberry yad mednafen virtualbox virtualbox-host-dkms filezilla php chromium composer dbeaver nicotine+ yt-dlp docker
 
 # Enabling services
-systemctl enable thermald earlyoom apparmor firewalld 
+systemctl enable thermald earlyoom apparmor firewalld docker
+
+# Installing computer specific packages
+pacman -S --noconfirm pam-u2f
 
 # Wine dependencies
 pacman -S --needed --noconfirm wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
@@ -236,7 +239,7 @@ if [[ "$2" == "gtk" ]]; then
 fi
 
 # Installing the rest of AUR packages with user link
-sudo -u link yay -S --noconfirm android-studio protontricks eclipse-jee mednaffe 
+sudo -u link yay -S --noconfirm android-studio protontricks eclipse-jee mednaffe phpstorm
 
 # Linking yt-dlp to youtube-dl
 ln -s /usr/bin/yt-dlp /usr/bin/youtube-dl

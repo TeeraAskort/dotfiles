83a19c6593142c3ab1252bb49bc926fe87b01ad0
Andreu Furió Benavent <sariaaskort@tuta.io>
2021-12-13 12:18:53 +0000

    Added pipewire

--- desktop/arch/desktop_install.sh
+++ desktop/arch/desktop_install.sh
@@ -58,7 +58,7 @@ pacman -S --noconfirm networkmanager openssh xdg-user-dirs haveged intel-ucode
 systemctl enable NetworkManager haveged
 
 # Installing sound libraries
-pacman -S --noconfirm alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth
+pacman -S --noconfirm alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pipewire wireplumber
 
 # Installing filesystem libraries
 pacman -S --noconfirm dosfstools ntfs-3g btrfs-progs exfatprogs gptfdisk fuse2 fuse3 fuseiso sshfs cryptsetup f2fs-tools xfsprogs

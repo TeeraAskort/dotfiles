1ca91f787ae16a173e96ab57ebe4dfbd7eb0f251
Alderaeney <alderaeney@gmail.com>
2022-01-28 17:19:42 +0100

    Added slick greeter to 340s config

--- 340s/arch/desktop_install.sh
+++ 340s/arch/desktop_install.sh
@@ -158,11 +158,14 @@ if [[ "$1" == "gnome" ]]; then
 fi
 
 if [[ "$1" == "cinnamon" ]] || [[ "$1" == "mate" ]] || [[ "$1" == "xfce" ]]; then
-	# Installing lightdm-gtk-greeter
-	pacman -S --noconfirm lightdm-gtk-greeter
+	# Installing lightdm-slick-greeter
+	pacman -S --noconfirm lightdm-slick-greeter
+
+	# Installing lightdm-settings
+	sudo -u aurbuilder yay -S --noconfirm lightdm-settings
 
 	# Change lightdm theme
-	sed -i "s/^#greeter-session=.*$/greeter-session=lightdm-gtk-greeter/" /etc/lightdm/lightdm.conf
+	sed -i "s/^#greeter-session=.*$/greeter-session=lightdm-slick-greeter/" /etc/lightdm/lightdm.conf
 
 fi
 

af0274ee98c767a60e9282d8fa27d7d7f870fe12
Alderaeney <alderaeney@gmail.com>
2022-02-12 08:47:46 +0100

    Changed to pipewire

--- 340s/arch/desktop_install.sh
+++ 340s/arch/desktop_install.sh
@@ -79,7 +79,7 @@ pacman -S --noconfirm networkmanager openssh xdg-user-dirs haveged intel-ucode b
 systemctl enable NetworkManager haveged bluetooth
 
 # Installing sound libraries
-pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
 
 # Enabling pipewire service
 sudo -u link systemctl --user enable pipewire.socket
--- desktop/arch/desktop_install.sh
+++ desktop/arch/desktop_install.sh
@@ -80,7 +80,7 @@ pacman -S --noconfirm networkmanager openssh xdg-user-dirs haveged intel-ucode
 systemctl enable NetworkManager haveged
 
 # Installing sound libraries
-pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
 
 # Enabling pipewire service
 sudo -u link systemctl --user enable pipewire.socket

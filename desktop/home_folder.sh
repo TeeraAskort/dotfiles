36a45fd3df8a95f57e46b05f9f1068fd12eed299
Alderaeney <alderaeney@gmail.com>
2022-02-12 21:54:06 +0100

    Removed pipewire config

--- desktop/home_folder.sh
+++ desktop/home_folder.sh
@@ -91,17 +91,6 @@ if command -v pipewire &> /dev/null ; then
 	fi
 fi
 
-## Configuring pipewire
-if command -v pipewire &> /dev/null ; then
-	cd $directory/../common/
-	cp -r pipewire ~/.config/
-	systemctl --user restart pipewire.service pipewire-pulse.socket
-	if command -v wireplumber &> /dev/null ; then
-		cp -r wireplumber ~/.config/
-		systemctl --user restart wireplumber
-	fi
-fi
-
 ## Configuring vim/neovim
 cp $directory/dotfiles/.vimrc ~
 mkdir -p ~/.config/nvim/

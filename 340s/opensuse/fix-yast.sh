f5f67941ec010b80b7d5cd551372db77446962c0
Alderaeney <alderaeney@gmail.com>
2022-02-26 22:41:58 +0100

    Fixed issue with fix-yast.sh

--- 340s/opensuse/fix-yast.sh
+++ 340s/opensuse/fix-yast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i "s/\/usr\/bin\/xdg-su -c \/usr\/sbin\/yast2/\/usr\/local\/sbin\/yast2_polkit/g" /usr/share/applications/org.opensuse.YaST.desktop
+sed -i "s/\/usr\/bin\/xdg-su -c \/sbin\/yast2/\/usr\/local\/sbin\/yast2_polkit/g" /usr/share/applications/org.opensuse.YaST.desktop
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
@@ -10,7 +10,7 @@ cd /usr/share/applications/YaST2
 for file in $(ls /usr/share/applications/YaST2); do 
 	if [ -f $file ]; then
 		sed -i "s/\"//g" $file
-		sed -i "s/\/usr\/bin\/xdg-su -c \/usr\/sbin\/yast2/\/usr\/local\/sbin\/yast2_polkit/g" $file
+		sed -i "s/\/usr\/bin\/xdg-su -c \/sbin\/yast2/\/usr\/local\/sbin\/yast2_polkit/g" $file
 	fi 
 done
 

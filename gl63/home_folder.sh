dfd0402f5b9adfdf72e189a44d65915ccedf6b66
Alderaeney <alderaeney@gmail.com>
2022-03-01 12:03:49 +0100

    Removed docker thing

--- gl63/home_folder.sh
+++ gl63/home_folder.sh
@@ -149,11 +149,6 @@ else
 	sudo udevadm control --reload-rules
 fi
 
-## Configuring docker
-cd $directory/../common
-sudo systemctl restart docker
-sudo docker-compose -f compose.yml up -d --build
-
 ## Copying ssh key
 mkdir ~/.ssh
 cp ~/Documentos/id_ed25519* ~/.ssh

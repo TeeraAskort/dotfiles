e7c5f55bac728d4ed88c7682b73e3a49fd5f8c75
Alderaeney <alderaeney@gmail.com>
2022-03-22 08:25:05 +0000

    Added noconfirm to chaotic installation

--- 340s/arch/desktop_install.sh
+++ 340s/arch/desktop_install.sh
@@ -45,7 +45,7 @@ sed -i "s/#ParallelDownloads/ParallelDownloads/g" /etc/pacman.conf
 # Adding Chaotic AUR repo
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Adding repos
 cat >> /etc/pacman.conf <<EOF

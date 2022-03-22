2a147af1678486a3de0ae39473d1bf8ae5cc1161
Alderaeney <alderaeney@gmail.com>
2022-03-22 07:44:21 +0000

    Changed installation of aur packages

--- gl63/arch/desktop_install.sh
+++ gl63/arch/desktop_install.sh
@@ -245,7 +245,7 @@ pacman -S --noconfirm gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plu
 pacman -S --noconfirm gimp gimp-help-es
 
 # Installing required packages
-pacman -S --noconfirm jdk11-openjdk dolphin-emu discord telegram-desktop flatpak wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald apparmor gamemode lib32-gamemode firefox firefox-i18n-es-es gparted noto-fonts gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse firewalld obs-studio neovim nodejs npm python-pynvim libfido2 yad mednafen chromium nicotine+ yt-dlp pcsx2 syncthing zram-generator home_Alderaeney_Arch/strawberry-qt5 docker docker-compose
+pacman -S --noconfirm jdk11-openjdk dolphin-emu discord telegram-desktop flatpak wine-staging winetricks wine-gecko wine-mono lutris zsh zsh-autosuggestions zsh-syntax-highlighting noto-fonts-cjk papirus-icon-theme steam thermald apparmor gamemode lib32-gamemode firefox firefox-i18n-es-es gparted noto-fonts gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ttf-hack lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse firewalld obs-studio neovim nodejs npm python-pynvim libfido2 yad mednafen chromium nicotine+ yt-dlp pcsx2 syncthing zram-generator home_Alderaeney_Arch/strawberry-qt5 docker docker-compose protontricks mednaffe android-studio postman-bin jdownloader2 visual-studio-code-bin pfetch minigalaxy minecraft-launcher
 
 # Enabling services
 systemctl enable thermald apparmor firewalld syncthing@link.service systemd-oomd.service
@@ -266,7 +266,7 @@ pacman -S --noconfirm pam-u2f intel-undervolt
 pacman -S --needed --noconfirm wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader openssl-1.0
 
 # Installing AUR packages
-sudo -u aurbuilder yay -S --noconfirm dxvk-bin jdownloader2 visual-studio-code-bin pfetch minigalaxy minecraft-launcher razergenie openrazer-meta postman-bin # touchegg
+sudo -u aurbuilder yay -S --noconfirm dxvk-bin razergenie openrazer-meta # touchegg
 
 # Adding user to plugdev group
 usermod -aG plugdev link
@@ -294,9 +294,6 @@ if [[ "$2" == "gtk" ]]; then
 		echo "QT_STYLE_OVERRIDE=adwaita-dark" | tee -a /etc/environment
 	fi
 fi
-
-# Installing the rest of AUR packages with user link
-sudo -u link yay -S --noconfirm protontricks mednaffe android-studio
 
 # Linking yt-dlp to youtube-dl
 ln -s /usr/bin/yt-dlp /usr/bin/youtube-dl

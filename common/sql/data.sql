8204f217952cc18e401cb3e6ff8bc85c0c593401
Alderaeney <alderaeney@gmail.com>
2022-03-24 14:17:36 +0100

    Fixed data.sql

--- common/sql/data.sql
+++ common/sql/data.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE mangaloide;
 CREATE USER 'mangaloide'@'%' IDENTIFIED BY 'mangaloide';
-GRANT ALL PRIVILEGES ON mangaloide TO 'mangaloide'@'%' IDENTIFIED BY 'mangaloide';
+GRANT ALL PRIVILEGES ON mangaloide.* TO 'mangaloide'@'%' IDENTIFIED BY 'mangaloide';

801ba5aae980ec68ba8e0f6f6d3c4cd2894de907
Alderaeney <alderaeney@gmail.com>
2022-04-25 14:47:28 +0200

    Fixed path

--- 340s/home_folder.sh
+++ 340s/home_folder.sh
@@ -376,7 +376,7 @@ if [ $(lsb_release -is) == "Arch" ]; then
 
 	mkdir ~/.config/autostart
 
-cat > .config/autostart/apparmor-notify.desktop <<EOF
+cat > ~/.config/autostart/apparmor-notify.desktop <<EOF
 [Desktop Entry]
 Type=Application
 Name=AppArmor Notify

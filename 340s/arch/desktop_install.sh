da2fcb15f0c4926f3ab28e05ce468a770508cf70
Alderaeney <alderaeney@gmail.com>
2022-05-04 16:58:01 +0200

    Changed to pulseaudio

--- 340s/arch/desktop_install.sh
+++ 340s/arch/desktop_install.sh
@@ -87,10 +87,10 @@ pacman -S --noconfirm networkmanager openssh xdg-user-dirs haveged intel-ucode b
 systemctl enable NetworkManager haveged bluetooth
 
 # Installing sound libraries
-pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
+pacman -S --noconfirm alsa-utils alsa-plugins pipewire lib32-pipewire wireplumber pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
 
 # Configuring pulseaudio
-# sed -i "s/load-module module-suspend-on-idle/#load-module module-suspend-on-idle/g" /etc/pulse/default.pa
+sed -i "s/load-module module-suspend-on-idle/#load-module module-suspend-on-idle/g" /etc/pulse/default.pa
 
 # Enabling pipewire service
 sudo -u link systemctl --user enable pipewire.socket

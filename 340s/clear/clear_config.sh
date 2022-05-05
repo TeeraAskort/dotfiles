99e73041c72585f8ead4f88e8c347f6138bbf42d
Alderaeney <alderaeney@gmail.com>
2022-05-05 14:35:07 +0200

    Updated clear config

--- 340s/clear/clear_config.sh
+++ 340s/clear/clear_config.sh
@@ -8,7 +8,7 @@ directory="$(dirname $_script)"
 swupd update
 
 # Installing basic packages
-swupd bundle-add lutris games vim neovim mpv zsh java-basic earlyoom flatpak nodejs-basic kernel-native-dkms wine zip storage-utils unzip docker-compose yarn desktop-apps
+swupd bundle-add lutris games vim neovim mpv zsh java-basic earlyoom flatpak nodejs-basic kernel-native-dkms wine zip storage-utils unzip docker-compose yarn desktop-apps fonts-basic
 
 # Installing flatpak applications
 flatpak install flathub -y io.lbry.lbry-app org.jdownloader.JDownloader com.github.AmatCoder.mednaffe org.telegram.desktop com.getpostman.Postman net.pcsx2.PCSX2 org.desmume.DeSmuME org.strawberrymusicplayer.strawberry com.visualstudio.code com.discordapp.Discord com.mojang.Minecraft com.google.Chrome sh.ppy.osu com.nextcloud.desktopclient.nextcloud com.obsproject.Studio org.gimp.GIMP io.github.sharkwouter.Minigalaxy

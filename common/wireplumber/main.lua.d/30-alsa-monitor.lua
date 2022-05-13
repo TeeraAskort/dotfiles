0d9e9f1bc2c02748f9d2da4462e2310de3a786d4
Alderaeney <alderaeney@gmail.com>
2022-05-13 22:39:30 +0200

    Finally fixed wireplumber config for version 0.4.10

--- common/wireplumber/main.lua.d/30-alsa-monitor.lua
+++ common/wireplumber/main.lua.d/30-alsa-monitor.lua
@@ -1,43 +1,8 @@
 alsa_monitor = {}
 alsa_monitor.properties = {}
-alsa_monitor.rules = {
-{
-    matches = {
-      {
-        -- Matches all sources.
-        { "node.name", "matches", "alsa_input.*" },
-      },
-      {
-        -- Matches all sinks.
-        { "node.name", "matches", "alsa_output.*" },
-      },
-    },
-    apply_properties = {
-      --["node.nick"]              = "My Node",
-      --["priority.driver"]        = 100,
-      --["priority.session"]       = 100,
-      ["node.pause-on-idle"]     = false,
-      --["resample.quality"]       = 4,
-      --["channelmix.normalize"]   = false,
-      --["channelmix.mix-lfe"]     = false,
-      --["audio.channels"]         = 2,
-      --["audio.format"]           = "S16LE",
-      --["audio.rate"]             = 44100,
-      --["audio.position"]         = "FL,FR",
-      --["api.alsa.period-size"]   = 1024,
-      ["api.alsa.headroom"]      = 128,
-      --["api.alsa.disable-mmap"]  = false,
-      ["api.alsa.disable-batch"] = true,
-      ["session.suspend-timeout-seconds"] = 0
-    },
-  },
-}
+alsa_monitor.rules = {}
 
 function alsa_monitor.enable()
-  if not alsa_monitor.enabled then
-    return
-  end
-
   -- The "reserve-device" module needs to be loaded for reservation to work
   if alsa_monitor.properties["alsa.reserve"] then
     load_module("reserve-device")

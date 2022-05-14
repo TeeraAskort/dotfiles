ebe5286398f11f876a2156347ea606f1b1ef083c
Alderaeney <alderaeney@gmail.com>
2022-05-14 22:37:14 +0200

    Added more packages; Added post install

---
+++ 340s/silverblue/post_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Installing packages from rpmfusion
+rpm-ostree install unrar ffmpeg ffmpegthumbnailer mozilla-openh264 gstreamer1-plugin-openh264 gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-libav gstreamer1-plugins-good-extras gstreamer1-plugins-bad-free-extras
+
+# Enabling services
+systemctl enable docker
+
+# Starting services
+systemctl start docker
+
+# Adding user to docker group
+user="$SUDO_USER"
+usermod -aG docker $user
+
+# Adding user to plugdev group
+user="$SUDO_USER"
+usermod -aG plugdev $user

1c53a7dca58f2a86fb0d34353cdc14f33cd91daf
Alderaeney <alderaeney@gmail.com>
2022-05-14 22:33:15 +0200

    Added more packages

--- 340s/silverblue/pre_install.sh
+++ 340s/silverblue/pre_install.sh
@@ -21,7 +21,7 @@ rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rele
 hostnamectl set-hostname link-340s
 
 # First package installation
-rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks papirus-icon-theme java-11-openjdk-devel papirus-icon-theme java-11-openjdk-devel neovim python-neovim seahorse nodejs npm yarnpkg docker-ce docker-ce-cli containerd.io docker-compose brasero unzip zip 
+rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks papirus-icon-theme java-11-openjdk-devel papirus-icon-theme java-11-openjdk-devel neovim python-neovim seahorse nodejs npm yarnpkg docker-ce docker-ce-cli containerd.io docker-compose brasero unzip zip simple-scan lutris
 
 # Updating the system
 rpm-ostree upgrade
@@ -32,8 +32,11 @@ rpm-ostree install "https://github.com/mongodb-js/compass/releases/download/v1.3
 # Adding flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Uninstalling flatpak applications
+flatpak remove -y org.fedoraproject.MediaWriter org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.Connections org.gnome.Contacts org.gnome.Evince org.gnome.Extensions org.gnome.Logs org.gnome.Maps org.gnome.NautilusPreviewer org.gnome.TextEditor org.gnome.Weather org.gnome.baobab org.gnome.clocks org.gnome.eog org.gnome.font-viewer
+
 # Installing flatpak applications
-flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark com.mojang.Minecraft org.jdownloader.JDownloader sh.ppy.osu net.pcsx2.PCSX2 com.valvesoftware.Steam com.getpostman.Postman com.visualstudio.code com.github.AmatCoder.mednaffe org.telegram.desktop com.discordapp.Discord org.strawberrymusicplayer.strawberry org.DolphinEmu.dolphin-emu io.mpv.Mpv io.github.sharkwouter.Minigalaxy xyz.z3ntu.razergenie org.gnome.Aisleriot org.gnome.Mahjongg org.desmume.DeSmuME com.obsproject.Studio org.gnome.Boxes org.nicotine_plus.Nicotine org.deluge_torrent.deluge com.nextcloud.desktopclient.nextcloud com.google.Chrome org.gimp.GIMP
+flatpak install flathub -y org.gtk.Gtk3theme.Adwaita-dark com.mojang.Minecraft org.jdownloader.JDownloader sh.ppy.osu net.pcsx2.PCSX2 com.valvesoftware.Steam com.getpostman.Postman com.visualstudio.code com.github.AmatCoder.mednaffe org.telegram.desktop com.discordapp.Discord org.strawberrymusicplayer.strawberry org.DolphinEmu.dolphin-emu io.mpv.Mpv io.github.sharkwouter.Minigalaxy xyz.z3ntu.razergenie org.gnome.Aisleriot org.gnome.Mahjongg org.desmume.DeSmuME com.obsproject.Studio org.gnome.Boxes org.nicotine_plus.Nicotine org.deluge_torrent.deluge com.nextcloud.desktopclient.nextcloud com.google.Chrome org.gimp.GIMP org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.Contacts org.gnome.Evince org.gnome.Logs org.gnome.Maps org.gnome.NautilusPreviewer org.gnome.TextEditor org.gnome.Weather org.gnome.baobab org.gnome.clocks org.gnome.eog org.gnome.font-viewer org.gnome.Geary org.gnome.FileRoller
 
 # Configuring hibernate
 mkdir -p /etc/dracut.conf.d

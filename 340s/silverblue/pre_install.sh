9235ac31b9fe9032ffdbf3431098b6e14ba649c3
Alderaeney <alderaeney@gmail.com>
2022-05-15 00:01:42 +0200

    Fixed silverblue install

--- 340s/silverblue/pre_install.sh
+++ 340s/silverblue/pre_install.sh
@@ -8,9 +8,6 @@ user=$SUDO_USER
 
 # Adding repos
 
-## Openrazer repo
-curl -L "https://download.opensuse.org/repositories/hardware:razer/Fedora_35/hardware:razer.repo" > /etc/yum.repos.d/hardware:razer.repo
-
 ## Better_fonts repo
 curl -L "https://copr.fedorainfracloud.org/coprs/hyperreal/better_fonts/repo/fedora-36/hyperreal-better_fonts-fedora-36.repo" > /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:hyperreal:better_fonts.repo
 
@@ -27,7 +24,7 @@ hostnamectl set-hostname link-340s
 rpm-ostree upgrade
 
 # First package installation
-rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks papirus-icon-theme java-11-openjdk-devel papirus-icon-theme java-11-openjdk-devel neovim python-neovim seahorse nodejs npm yarnpkg docker-ce docker-ce-cli containerd.io docker-compose brasero simple-scan lutris openrazer-meta pam-u2f pamu2fcfg fontconfig-font-replacements fontconfig-enhanced-defaults
+rpm-ostree install zsh zsh-syntax-highlighting zsh-autosuggestions vim gnome-tweaks papirus-icon-theme java-11-openjdk-devel papirus-icon-theme java-11-openjdk-devel neovim python-neovim seahorse nodejs npm yarnpkg docker-ce docker-ce-cli containerd.io docker-compose brasero simple-scan lutris pam-u2f pamu2fcfg fontconfig-font-replacements fontconfig-enhanced-defaults
 
 # Installing mongo compass
 rpm-ostree install "https://github.com/mongodb-js/compass/releases/download/v1.31.2/mongodb-compass-1.31.2.x86_64.rpm"
@@ -39,7 +36,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak remove -y org.fedoraproject.MediaWriter org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.Connections org.gnome.Contacts org.gnome.Evince org.gnome.Extensions org.gnome.Logs org.gnome.Maps org.gnome.NautilusPreviewer org.gnome.TextEditor org.gnome.Weather org.gnome.baobab org.gnome.clocks org.gnome.eog org.gnome.font-viewer
 
 # Installing flatpak applications
-flatpak install flathub -y org.gtk.Gtk3theme.Adwaita-dark com.mojang.Minecraft org.jdownloader.JDownloader sh.ppy.osu net.pcsx2.PCSX2 com.valvesoftware.Steam com.getpostman.Postman com.visualstudio.code com.github.AmatCoder.mednaffe org.telegram.desktop com.discordapp.Discord org.strawberrymusicplayer.strawberry org.DolphinEmu.dolphin-emu io.mpv.Mpv io.github.sharkwouter.Minigalaxy xyz.z3ntu.razergenie org.gnome.Aisleriot org.gnome.Mahjongg org.desmume.DeSmuME com.obsproject.Studio org.gnome.Boxes org.nicotine_plus.Nicotine org.deluge_torrent.deluge com.nextcloud.desktopclient.nextcloud com.google.Chrome org.gimp.GIMP org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.Contacts org.gnome.Evince org.gnome.Logs org.gnome.Maps org.gnome.NautilusPreviewer org.gnome.TextEditor org.gnome.Weather org.gnome.baobab org.gnome.clocks org.gnome.eog org.gnome.font-viewer org.gnome.Geary org.gnome.FileRoller
+flatpak install flathub -y org.gtk.Gtk3theme.Adwaita-dark com.mojang.Minecraft org.jdownloader.JDownloader sh.ppy.osu net.pcsx2.PCSX2 com.valvesoftware.Steam com.getpostman.Postman com.visualstudio.code com.github.AmatCoder.mednaffe org.telegram.desktop com.discordapp.Discord org.strawberrymusicplayer.strawberry org.DolphinEmu.dolphin-emu io.mpv.Mpv io.github.sharkwouter.Minigalaxy org.gnome.Aisleriot org.gnome.Mahjongg org.desmume.DeSmuME com.obsproject.Studio org.gnome.Boxes org.nicotine_plus.Nicotine org.deluge_torrent.deluge com.nextcloud.desktopclient.nextcloud com.google.Chrome org.gimp.GIMP org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.Contacts org.gnome.Evince org.gnome.Logs org.gnome.Maps org.gnome.NautilusPreviewer org.gnome.TextEditor org.gnome.Weather org.gnome.baobab org.gnome.clocks org.gnome.eog org.gnome.font-viewer org.gnome.Geary org.gnome.FileRoller
 
 # Configuring hibernate
 mkdir -p /etc/dracut.conf.d

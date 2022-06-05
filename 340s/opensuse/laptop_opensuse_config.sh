110b2a83c3167ef303787a5326fb371abc02158e
Alderaeney <alderaeney@gmail.com>
2022-06-05 19:15:45 +0200

    Fixed compass dependecy installation

--- 340s/opensuse/laptop_opensuse_config.sh
+++ 340s/opensuse/laptop_opensuse_config.sh
@@ -85,7 +85,7 @@ if [ "$1" == "gnome" ] || [ "$1" == "kde" ] || [ "$1" == "plasma" ] || [ "$1" ==
 	zramswapon
 
 	# Installing mongodb compass
-	zypper in -y lsb-core-noarch bc ed libfl2 libnotify-tools m4 make make-lang parallel-printer-support site-config at binutils cups-client libcupscgi1 libcupsimage2 libpoppler-cpp0 poppler-tools cups cups-filters ghostscript lsb
+	zypper in -y at bc libHX32 libnotify-tools lsb
 	curl -L "https://github.com/mongodb-js/compass/releases/download/v1.32.1/mongodb-compass-1.32.1.x86_64.rpm" > compass.rpm
 	rpm -ivh --nodeps compass.rpm
 	rm compass.rpm

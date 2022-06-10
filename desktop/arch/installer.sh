ef0a7902885b1fc83fe105c3fd15623bfe26c9f3
Alderaeney <alderaeney@gmail.com>
2022-06-10 15:55:32 +0200

    Changed to zen kernel

--- desktop/arch/installer.sh
+++ desktop/arch/installer.sh
@@ -47,7 +47,7 @@ if [[ "$1" == "plasma" ]] || [[ "$1" == "kde" ]] || [[ "$1" == "gnome" ]] || [[ 
 	pacman -Sy --noconfirm archlinux-keyring
 
 	# Install base system
-	pacstrap /mnt base base-devel linux-firmware linux linux-headers lvm2 efibootmgr btrfs-progs vim git xfsprogs 
+	pacstrap /mnt base base-devel linux-firmware linux-zen linux-zen-headers lvm2 efibootmgr btrfs-progs vim git xfsprogs 
 
 	# Executing partprobe
 	partprobe /dev/nvme0n1

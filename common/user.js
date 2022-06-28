d6e388ca6500ea5aaa1c3c76ccf8afedcbe4e326
Alderaeney <alderaeney@gmail.com>
2022-06-28 20:29:23 +0200

    Enabling gfx.webrender.all

--- common/user.js
+++ common/user.js
@@ -1,4 +1,4 @@
-// user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.all", true);
 user_pref("media.hardwaremediakeys.enabled", true);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("media.ffmpeg.vaapi-drm-display.enabled", true);

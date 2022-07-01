e75ba1673e124264d3e16d8ab5326767ef81dbf1
Alderaeney <alderaeney@gmail.com>
2022-07-01 23:11:30 +0200

    Removed keyring config

--- gl63/arch/desktop_install.sh
+++ gl63/arch/desktop_install.sh
@@ -337,14 +337,6 @@ if [[ "$1" == "gnome" ]]; then
 	# Adding ssh-askpass env var
 	echo "SSH_ASKPASS=/usr/lib/seahorse/ssh-askpass" | tee -a /etc/environment
 
-	# Adding gnome-keyring to pam
-	echo "password optional pam_gnome_keyring.so" | tee -a /etc/pam.d/passwd
-
-	# Add keyring unlock on login
-	awk 'FNR==NR{ if (/auth/) p=NR; next} 1; FNR==p{ print "auth       optional     pam_gnome_keyring.so" }' /etc/pam.d/login /etc/pam.d/login | tee $directory/tmp
-	echo "session    optional     pam_gnome_keyring.so auto_start" | tee -a $directory/tmp
-	mv $directory/tmp /etc/pam.d/login
-
 elif [[ "$1" == "xfce" ]]; then
 	# Adding xprofile to user link
 	sudo -u link echo "xcape -e 'Super_L=Control_L|Escape'" | tee -a /home/link/.xprofile
@@ -382,13 +374,6 @@ elif [[ "$1" == "cinnamon" ]]; then
 	# Adding ssh-askpass env var
 	echo "SSH_ASKPASS=/usr/lib/seahorse/ssh-askpass" | tee -a /etc/environment
 
-	# Adding gnome-keyring to pam
-	echo "password optional pam_gnome_keyring.so" | tee -a /etc/pam.d/passwd
-
-	# Add keyring unlock on login
-	awk 'FNR==NR{ if (/auth/) p=NR; next} 1; FNR==p{ print "auth       optional     pam_gnome_keyring.so" }' /etc/pam.d/login /etc/pam.d/login | tee $directory/tmp
-	echo "session    optional     pam_gnome_keyring.so auto_start" | tee -a $directory/tmp
-	mv $directory/tmp /etc/pam.d/login
 fi
 
 # Copying dotfiles folder to link

d6be87ef6af8957a95006a7c9ed51205dd4e37f7
Alderaeney <alderaeney@gmail.com>
2022-07-26 12:41:04 +0200

    Changed nativescript to ionic

--- desktop/home_folder.sh
+++ desktop/home_folder.sh
@@ -117,12 +117,12 @@ unzip ~/Documentos/fonts2.zip
 # Installing NPM packages
 if command -v rpm-ostree &> /dev/null; then
 	npm config set prefix '~/.node_packages'
-	npm install -g nativescript
+	npm install -g @ionic/cli
 	if [[ "$XDG_CURRENT_DESKTOP" == "KDE" ]] && command -v lsb_release &> /dev/null && [[ $(lsb_release -is) != "openSUSE" ]]; then
 		npm install -g bash-language-server
 	fi
 else
-	sudo npm install -g nativescript
+	sudo npm install -g @ionic/cli
 	if [[ "$XDG_CURRENT_DESKTOP" == "KDE" ]] && command -v lsb_release &> /dev/null && [[ $(lsb_release -is) != "openSUSE" ]]; then
 		sudo npm install -g bash-language-server
 	fi

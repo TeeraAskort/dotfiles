a589c01f3192aca016d3a46674758b0a8c008ca2
Alderaeney <alderaeney@gmail.com>
2022-09-02 22:52:06 +0200

    Added pacman hook for nvidia

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -334,6 +334,26 @@ EOF
 # Auto enable bluetooth on startup
 sed -i "s/#AutoEnable=false/AutoEnable=true/g" /etc/bluetooth/main.conf
 
+# Adding nvidia pacman hook
+mkdir -p /etc/pacman.d/hooks
+cat > /etc/pacman.d/hooks/nvidia.hook <<EOF
+[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+Target=linux
+# Change the linux part above and in the Exec line if a different kernel is used
+
+[Action]
+Description=Update NVIDIA module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+EOF
+
 # Cleaning orphans
 pacman -Qtdq | pacman -Rns --noconfirm -
 

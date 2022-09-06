bb55d6294b1fd8f3a78055107f20ecb86e17e487
Alderaeney <alderaeney@gmail.com>
2022-09-06 20:36:19 +0000

    Updated arch config

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -269,7 +269,7 @@ mount-point = /var/compressed
 EOF
 
 # Installing computer specific packages
-pacman -S --noconfirm pam-u2f
+pacman -S --noconfirm pam-u2f touchegg
 
 # Wine dependencies
 pacman -S --noconfirm --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
@@ -354,7 +354,8 @@ NeedsTargets
 Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
 EOF
 
-echo "PROTON_ENABLE_NVAPI=1" | tee -a /etc/environment
+# Copying nvapi script
+cp $directory/../dotfiles/nvapi /usr/bin
 
 # Cleaning orphans
 pacman -Qtdq | pacman -Rns --noconfirm -

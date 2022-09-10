1140d80b7ce096b3a8899241d4455a26a6295b7b
Alderaeney <alderaeney@gmail.com>
2022-09-10 14:25:04 +0200

    Added repository refresh

--- gp76/opensuse/first_boot.sh
+++ gp76/opensuse/first_boot.sh
@@ -3,6 +3,9 @@
 # Adding nvidia repo
 zypper addrepo https://download.nvidia.com/opensuse/tumbleweed NVIDIA
 
+# Refreshing repositories
+zypper --gpg-auto-import-keys refresh
+
 # Install nvidia drivers
 zypper in --auto-agree-with-licenses -y x11-video-nvidiaG06
 

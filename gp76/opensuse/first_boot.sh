11674479538886edfa0cdea5814b8b3b8125476c
Alderaeney <alderaeney@gmail.com>
2022-09-11 19:28:05 +0200

    Added suspend options to nvidia drivers

--- gp76/opensuse/first_boot.sh
+++ gp76/opensuse/first_boot.sh
@@ -9,4 +9,12 @@ zypper --gpg-auto-import-keys refresh
 # Install nvidia drivers
 zypper in --auto-agree-with-licenses -y x11-video-nvidiaG06
 
+# Enabling services
+systemctl enable nvidia-suspend nvidia-hibernate
+
+# Adding nvidia options
+cat > /etc/modprobe.d/nvidia-power-management.conf <<EOF
+options nvidia NVreg_PreserveVideoMemoryAllocations=1
+EOF
+
 echo "Nvidia drivers installed, reboot the computer"

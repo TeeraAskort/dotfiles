a7ecd00e8b86501f41fe75fa24a330132579b6d5
Alderaeney <alderaeney@gmail.com>
2022-09-12 11:33:56 +0200

    Added nvidia modules

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -205,7 +205,7 @@ cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermar
 # Configuring mkinitcpio
 pacman -S --noconfirm --needed lvm2
 sed -i "s/udev autodetect modconf block filesystems/udev plymouth autodetect modconf block plymouth-encrypt lvm2 filesystems/g" /etc/mkinitcpio.conf
-# sed -i "s/MODULES=()/MODULES=()/g" /etc/mkinitcpio.conf
+sed -i "s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g" /etc/mkinitcpio.conf
 mkinitcpio -P
 
 # Install and configure systemd-boot

464c4da957f3627dbf2fd21f850eed48f89c9d4e
Alderaeney <alderaeney@gmail.com>
2022-09-12 15:04:35 +0200

    Removed vmd

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -206,7 +206,7 @@ cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermar
 # Configuring mkinitcpio
 pacman -S --noconfirm --needed lvm2
 sed -i "s/udev autodetect modconf block filesystems/udev plymouth autodetect modconf block plymouth-encrypt lvm2 filesystems/g" /etc/mkinitcpio.conf
-sed -i "s/MODULES=()/MODULES=(i915 vmd)/g" /etc/mkinitcpio.conf
+sed -i "s/MODULES=()/MODULES=(i915)/g" /etc/mkinitcpio.conf
 mkinitcpio -P
 
 # Install and configure systemd-boot

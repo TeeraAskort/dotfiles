e8ef710180fb83a9d6ea13d3b3d86cc78fcc4c9b
Alderaeney <alderaeney@gmail.com>
2022-10-09 10:02:08 +0200

    Removed touchegg

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -155,10 +155,10 @@ if [[ "$1" == "cinnamon" ]]; then
 
 elif [[ "$1" == "gnome" ]]; then
 	# Install GNOME
-	pacman -S --noconfirm extra/gnome gnome-tweaks gnome-nettool gnome-mahjongg aisleriot ffmpegthumbnailer gtk-engine-murrine geary deluge deluge-gtk libappindicator-gtk3 libnotify webp-pixbuf-loader libgepub libgsf libopenraw brasero gnome-themes-extra xdg-desktop-portal xdg-desktop-portal-gnome gnome-software-packagekit-plugin gdm-plymouth gnome-browser-connector simple-scan gnome-boxes seahorse gnome-text-editor-git libsecret gvfs-google mutter-performance python-nautilus touchegg
+	pacman -S --noconfirm extra/gnome gnome-tweaks gnome-nettool gnome-mahjongg aisleriot ffmpegthumbnailer gtk-engine-murrine geary deluge deluge-gtk libappindicator-gtk3 libnotify webp-pixbuf-loader libgepub libgsf libopenraw brasero gnome-themes-extra xdg-desktop-portal xdg-desktop-portal-gnome gnome-software-packagekit-plugin gdm-plymouth gnome-browser-connector simple-scan gnome-boxes seahorse gnome-text-editor-git libsecret gvfs-google mutter-performance python-nautilus # touchegg
 
 	# Enabling gdm
-	systemctl enable gdm touchegg
+	systemctl enable gdm # touchegg
 
 	# Removing unwanted packages
 	pacman -Rns --noconfirm gnome-music epiphany totem orca gedit

fd12f0190156b997de4d1d17e5760aadfbb0d9ad
Alderaeney <alderaeney@gmail.com>
2022-11-03 16:08:53 +0100

    Added efibootmgr UEFI menu entry creation

--- desktop/arch/desktop_install.sh
+++ desktop/arch/desktop_install.sh
@@ -213,6 +213,7 @@ sed -i "s/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g" /etc/def
 sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 cryptdevice=\/dev\/disk\/by-uuid\/$(blkid -s UUID -o value /dev/${rootDisk}p3):luks:allow-discards root=\/dev\/lvm\/root apparmor=1 lsm=lockdown,yama,apparmor splash rd.udev.log_priority=3 vt.global_cursor_default=0 kernel.yama.ptrace_scope=2\"/g" /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+efibootmgr --create --disk /dev/nvme1n1 --part 1 --loader /EFI/GRUB/grubx64.efi --label "Arch Linux" --unicode
 
 # bootctl install
 # mkdir -p /boot/loader/entries

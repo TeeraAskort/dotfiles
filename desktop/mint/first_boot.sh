1e451307d6d7bd225ab12d0f9e7e766a58b28aee
Alderaeney <alderaeney@gmail.com>
2023-01-08 23:14:30 +0100

    Removed kisak mesa repo

--- desktop/mint/first_boot.sh
+++ desktop/mint/first_boot.sh
@@ -7,8 +7,3 @@ apt full-upgrade -y
 
 ## Installing essential build tools and ppa
 apt-get install -y build-essential software-properties-common curl
-
-## Installing updated mesa
-add-apt-repository ppa:kisak/turtle -y
-apt update
-apt full-upgrade -y

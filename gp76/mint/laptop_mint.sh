59550f22946e30a0fa5c07fab2eec12df69645b0
Alderaeney <alderaeney@gmail.com>
2023-01-08 18:15:56 +0100

    Added apt update

--- gp76/mint/laptop_mint.sh
+++ gp76/mint/laptop_mint.sh
@@ -9,6 +9,7 @@ fi
 mkdir -pm755 /etc/apt/keyrings
 wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+apt update
 apt install --install-recommends winehq-stable -y
 
 ## Installing strawberry

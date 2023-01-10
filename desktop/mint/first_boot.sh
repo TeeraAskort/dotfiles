6f9e11775fca8846c7ffe49f76b0fbd019c825a6
Alderaeney <alderaeney@gmail.com>
2023-01-10 21:27:33 +0100

    Disabled sleep2idle

--- desktop/mint/first_boot.sh
+++ desktop/mint/first_boot.sh
@@ -13,3 +13,7 @@ echo 'deb http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xan
 wget -qO - https://dl.xanmod.org/gpg.key | apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
 apt update
 apt install -y linux-xanmod-x64v3
+
+## Disabling sleep2idle
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 mem_sleep_default=deep"/' /etc/default/grub
+update-grub

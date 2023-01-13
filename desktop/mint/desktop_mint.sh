1155e725bc31ced74ac7b8be56341053ae184c0b
Alderaeney <alderaeney@gmail.com>
2023-01-13 21:02:13 +0100

    Added zpaq to mint config

--- desktop/mint/desktop_mint.sh
+++ desktop/mint/desktop_mint.sh
@@ -53,7 +53,7 @@ curl -fsSL https://deb.nodesource.com/setup_lts.x |  bash -
 apt-get install -y nodejs
 
 ## Installing required packages
-apt install -y flatpak mpv zsh zsh-autosuggestions zsh-syntax-highlighting fonts-noto-cjk fonts-noto-color-emoji gamemode gparted vim neovim python3-neovim libfido2-1 mednafen mednaffe nextcloud-desktop zram-config gimp cups printer-driver-cups-pdf hplip libreoffice hunspell-en-us hunspell-es hunspell-ca aspell-ca aspell-es mythes-en-us mythes-es mythes-ca hyphen-en-us hyphen-es hyphen-ca zip unzip unrar p7zip lzop pigz pbzip2 bash-completion cryptsetup ntfs-3g neofetch yt-dlp thermald earlyoom solaar piper openjdk-17-jdk chromium-browser steam-installer
+apt install -y flatpak mpv zsh zsh-autosuggestions zsh-syntax-highlighting fonts-noto-cjk fonts-noto-color-emoji gamemode gparted vim neovim python3-neovim libfido2-1 mednafen mednaffe nextcloud-desktop zram-config gimp cups printer-driver-cups-pdf hplip libreoffice hunspell-en-us hunspell-es hunspell-ca aspell-ca aspell-es mythes-en-us mythes-es mythes-ca hyphen-en-us hyphen-es hyphen-ca zip unzip unrar p7zip lzop pigz pbzip2 bash-completion cryptsetup ntfs-3g neofetch yt-dlp thermald earlyoom solaar piper openjdk-17-jdk chromium-browser steam-installer zpaq
 
 ## Enabling services
 systemctl enable thermald earlyoom

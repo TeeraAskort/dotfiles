d3e2666997a367408bf369fbe3c6dcea8aed1f72
Alderaeney <alderaeney@gmail.com>
2023-02-02 21:39:21 +0000

    Removed conflicting config

--- gp76/nixos/configuration.nix
+++ gp76/nixos/configuration.nix
@@ -209,8 +209,6 @@ in
   hardware.nvidia = {
     powerManagement.enable = true;
     modesetting.enable = true;
-    nvidiaPersistenced = true;
-    powerManagement.enable = true;
   };
 
   # Enable CUPS to print documents.

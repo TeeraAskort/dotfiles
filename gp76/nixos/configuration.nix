6670982ff5ea8420cd0c79e225142b0b96c38ba3
Alderaeney <alderaeney@gmail.com>
2023-02-10 16:51:26 +0000

    Changed to pulseaudio

--- gp76/nixos/configuration.nix
+++ gp76/nixos/configuration.nix
@@ -236,20 +236,25 @@ in
   };
 
   # Enable sound.
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+    # enable = true;
+    # alsa.enable = true;
+    # alsa.support32Bit = true;
+    # pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+  # };
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
   };
-  hardware.pulseaudio.enable = false;
+  nixpkgs.config.pulseaudio = true;
+  hardware.bluetooth.enable = true;
 
   # Enabling xwayland
   # programs.xwayland.enable = true;

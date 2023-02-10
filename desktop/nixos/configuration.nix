f869a072f710a17aae189ebf227d2da995bc4190
Alderaeney <alderaeney@gmail.com>
2023-02-10 16:46:46 +0000

    Fixed issue

--- desktop/nixos/configuration.nix
+++ desktop/nixos/configuration.nix
@@ -222,7 +222,7 @@ in
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+  # };
   hardware.pulseaudio = {
     enable = true;
     support32Bit = true;

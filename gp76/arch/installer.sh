e2c9d47fbf0f4f215281c0bea1b2712e80df83e1
Alderaeney <alderaeney@gmail.com>
2023-05-04 20:05:59 +0200

    Changed filesystem

--- gp76/arch/installer.sh
+++ gp76/arch/installer.sh
@@ -33,7 +33,7 @@ if [[ "$1" == "gnome" ]] || [[ "$1" == "plasma" ]] || [[ "$1" == "kde" ]] || [[ 
 	lvcreate -l 100%FREE -n root lvm
 
 	# Format partitions
-	mkfs.btrfs -L root /dev/lvm/root
+	mkfs.ext4 -L root /dev/lvm/root
 	mkfs.vfat -F32 /dev/${torrentDisk}p1
 	mkswap /dev/lvm/swap
 	swapon /dev/lvm/swap

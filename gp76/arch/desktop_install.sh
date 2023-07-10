4b38c70b46966b4fdc2f2c0ac1cb42f26375e51b
Alderaeney <alderaeney@gmail.com>
2023-07-10 18:09:46 +0200

    Changed chaotic aur installation

--- gp76/arch/desktop_install.sh
+++ gp76/arch/desktop_install.sh
@@ -53,23 +53,23 @@ sed -i "s/#ParallelDownloads/ParallelDownloads/g" /etc/pacman.conf
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 
-# pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' # 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Adding home OBS repo
-cat >>/etc/pacman.conf <<EOF
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
-EOF
+# cat >>/etc/pacman.conf <<EOF
+# [chaotic-aur]
+# Include = /etc/pacman.d/chaotic-mirrorlist
+# EOF
 
 # Downloading the chaotic-aur mirrorlist
-until curl -L "https://aur.chaotic.cx/mirrorlist.txt" >/etc/pacman.d/chaotic-mirrorlist; do
-	echo "Retrying"
-done
-until pacman -Sy --noconfirm chaotic-mirrorlist chaotic-keyring; do
-	echo "Retrying"
-done
+# until curl -L "https://aur.chaotic.cx/mirrorlist.txt" >/etc/pacman.d/chaotic-mirrorlist; do
+# 	echo "Retrying"
+# done
+# until pacman -Sy --noconfirm chaotic-mirrorlist chaotic-keyring; do
+#	echo "Retrying"
+# done
 
-mv /etc/pacman.d/chaotic-mirrorlist.pacnew /etc/pacman.d/chaotic-mirrorlist
+# mv /etc/pacman.d/chaotic-mirrorlist.pacnew /etc/pacman.d/chaotic-mirrorlist
 
 until pacman -Syu --noconfirm; do
 	echo "Retrying"
